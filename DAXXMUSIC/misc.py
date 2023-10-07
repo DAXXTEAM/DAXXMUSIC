55c844f48987e163206e0a81f0d9f059a0ef3bf9
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-07 18:07:08 +0530

    Update misc.py

--- DAXXMUSIC/misc.py
+++ DAXXMUSIC/misc.py
@@ -58,7 +58,7 @@ async def sudo():
     if sudoers:
         for user_id in sudoers:
             SUDOERS.add(user_id)
-    LOGGER(__name__).info(f"Sudoers Loaded.")
+    LOGGER(__name__).info(f"𝗦𝗨𝗗𝗢 𝗨𝗦𝗘𝗥 𝗗𝗢𝗡𝗘✨🎋.")
 
 
 def heroku():
