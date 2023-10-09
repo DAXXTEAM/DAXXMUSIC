409751382d68693b207cda6620d71e30e4dee2ff
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-10 01:09:57 +0530

    Update mute.py

--- DAXXMUSIC/plugins/admins/mute.py
+++ DAXXMUSIC/plugins/admins/mute.py
@@ -1,31 +1,21 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from config import BANNED_USERS
-from strings import get_command
 from DAXXMUSIC import app
 from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.utils.database import is_muted, mute_on
+from DAXXMUSIC.utils.database import is_music_playing, music_off
 from DAXXMUSIC.utils.decorators import AdminRightsCheck
+from DAXXMUSIC.utils.inline import close_markup
+from config import BANNED_USERS
 
-# Commands
-MUTE_COMMAND = get_command("MUTE_COMMAND")
 
-
-@app.on_message(
-    filters.command(MUTE_COMMAND)
-    & filters.group
-    & ~filters.edited
-    & ~BANNED_USERS
-)
+@app.on_message(filters.command(["mute", "tmute"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
-async def mute_admin(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1 or message.reply_to_message:
-        return await message.reply_text(_["general_2"])
-    if await is_muted(chat_id):
-        return await message.reply_text(_["admin_5"])
-    await mute_on(chat_id)
-    await DAXXMUSIC.mute_stream(chat_id)
+async def pause_admin(cli, message: Message, _, chat_id):
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"])
+    await music_off(chat_id)
+    await DAXX.pause_stream(chat_id)
     await message.reply_text(
-        _["admin_6"].format(message.from_user.mention)
-      )
+        _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
