bd7d9ac3f554b3cec77b464924a44e6f89665bd5
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-10 18:53:50 +0530

    Update __main__.py

--- DAXXMUSIC/__main__.py
+++ DAXXMUSIC/__main__.py
@@ -50,7 +50,7 @@ async def init():
         pass
     await DAXX.decorators()
     LOGGER("DAXXMUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝐃𝐀𝐗𝐗♨️\n╚═════ஜ۩۞۩ஜ════╝"
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗗𝗔𝗫𝗫☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
