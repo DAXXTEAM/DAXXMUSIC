aeea725d609a3ad4b453d0884cb3571f32215b53
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-14 00:33:56 +0530

    Update play.py

--- DAXXMUSIC/plugins/play/play.py
+++ DAXXMUSIC/plugins/play/play.py
@@ -36,6 +36,7 @@ from config import BANNED_USERS, lyrical
             "vplayforce",
             "cplayforce",
             "cvplayforce",
+            "daxx",
         ]
     )
     & filters.group
