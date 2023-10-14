939247443c940cbabdc6cd07325ccdfde67ccc57
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-15 00:08:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ load_dotenv()
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-GPT_KEY = getenv("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")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
