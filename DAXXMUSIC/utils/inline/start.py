52c9f28099fd675a6911fd34c937b29e0a6eba20
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-21 11:15:00 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="DAXXMUSIC/assets/userinfo.png")
         ],
     ]
     return buttons
