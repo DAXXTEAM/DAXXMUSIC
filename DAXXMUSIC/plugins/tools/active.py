2f8a6b461cd31f065dbb73caeba384ea3d795ca8
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-01 15:45:43 +0530

    Update active.py

--- DAXXMUSIC/plugins/tools/active.py
+++ DAXXMUSIC/plugins/tools/active.py
@@ -44,7 +44,7 @@ async def activevc(_, message: Message):
         )
 
 
-@app.on_message(filters.command(["activev", "activevideo"]) & SUDOERS)
+@app.on_message(filters.command(["activev", "activevideo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
 async def activevi_(_, message: Message):
     mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ʟɪsᴛ...")
     served_chats = await get_active_video_chats()
