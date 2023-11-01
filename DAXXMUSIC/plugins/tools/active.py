f7050047c41efb08dc0893f657fa358a5c29d858
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-01 15:44:53 +0530

    Update active.py

--- DAXXMUSIC/plugins/tools/active.py
+++ DAXXMUSIC/plugins/tools/active.py
@@ -12,7 +12,7 @@ from DAXXMUSIC.utils.database import (
 )
 
 
-@app.on_message(filters.command(["activevc", "activevoice"]) & SUDOERS)
+@app.on_message(filters.command(["activevc", "activevoice"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
     served_chats = await get_active_chats()
