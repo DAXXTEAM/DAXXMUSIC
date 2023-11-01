dd54d6cc6ed76f7448fd369a50284d62eb28e13f
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-01 13:19:32 +0530

    Update play.py

--- DAXXMUSIC/plugins/play/play.py
+++ DAXXMUSIC/plugins/play/play.py
@@ -26,7 +26,7 @@ from config import BANNED_USERS, lyrical
 
 
 @app.on_message(
-   filters.command("play","vplay",prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
+   filters.command("play" ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
             
     & filters.group
     & ~BANNED_USERS
