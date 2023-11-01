9a7499c827b335ff3de7fbd048449aa97d189eec
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-01 14:26:37 +0530

    Update playmode.py

--- DAXXMUSIC/plugins/play/playmode.py
+++ DAXXMUSIC/plugins/play/playmode.py
@@ -8,7 +8,7 @@ from DAXXMUSIC.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["playmode", "mode"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["playmode" , "mode" ] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & ~BANNED_USERS)
 @language
 async def playmode_(client, message: Message, _):
     playmode = await get_playmode(message.chat.id)
