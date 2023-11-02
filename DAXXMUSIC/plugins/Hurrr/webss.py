d98f0c9c3bec9f3ca0ec6e261378486ffd5f9f7e
sᴀɢᴀʀˣᴀᴀʀᴏʜɪ <112709751+stkeditz@users.noreply.github.com>
2023-11-02 18:27:35 +0530

    Update webss.py

--- DAXXMUSIC/plugins/Hurrr/webss.py
+++ DAXXMUSIC/plugins/Hurrr/webss.py
@@ -5,7 +5,7 @@ from DAXXMUSIC import app
 from DAXXMUSIC.utils.errors import capture_err
 
 
-@app.on_message(filters.command("wss", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
+@app.on_message(filters.command("webss", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
 @capture_err
 async def take_ss(_, message: Message):
     try:
