6e57b2f1ca3d161210c07692cdac89f6118d58b7
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-02 14:53:25 +0530

    Update telegraph.py

--- DAXXMUSIC/plugins/tools/telegraph.py
+++ DAXXMUSIC/plugins/tools/telegraph.py
@@ -14,3 +14,39 @@ def ul(_, message):
             url = "https://telegra.ph" + x
 
         i.edit(f' 🇾ᴏᴜʀ🇹ᴇʟᴇɢʀᴀᴘʜ {url}')
+
+#image
+
+@app.on_message(filters.command(["image", "generate", "photo"]))
+async def pinterest(_, message):
+     chat_id = message.chat.id
+
+     try:
+       query= message.text.split(None,1)[1]
+     except:
+         return await message.reply("**ɢɪᴠᴇ ɪᴍᴀɢᴇ ɴᴀᴍᴇ ғᴏʀ sᴇᴀʀᴄʜ 🔍**")
+
+     images = get(f"https://pinterest-api-one.vercel.app/?q={query}").json()
+
+     media_group = []
+     count = 0
+
+     msg = await message.reply(f"sᴄʀᴀᴘɪɴɢ ɪᴍᴀɢᴇs ғʀᴏᴍ ᴘɪɴᴛᴇʀᴇᴛs...")
+     for url in images["images"][:6]:
+                  
+          media_group.append(InputMediaPhoto(media=url))
+          count += 1
+          await msg.edit(f"=> ᴏᴡᴏ sᴄʀᴀᴘᴇᴅ ɪᴍᴀɢᴇs {count}")
+
+     try:
+        
+        await DAXXMUSIC.send_media_group(
+                chat_id=chat_id, 
+                media=media_group,
+                reply_to_message_id=message.id)
+        return await msg.delete()
+
+     except Exception as e:
+           await msg.delete()
+           return await message.reply(f"ᴇʀʀᴏʀ : {e}")
+          
