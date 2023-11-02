721de385dbafca7e5d66dc355ad01d0bd9655839
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-02 15:04:38 +0530

    Update telegraph.py

--- DAXXMUSIC/plugins/tools/telegraph.py
+++ DAXXMUSIC/plugins/tools/telegraph.py
@@ -27,7 +27,7 @@ async def pinterest(_, message):
      except:
          return await message.reply("**ɢɪᴠᴇ ɪᴍᴀɢᴇ ɴᴀᴍᴇ ғᴏʀ sᴇᴀʀᴄʜ 🔍**")
 
-     images = set(f"https://pinterest-api-one.vercel.app/?q={query}").json()
+     images = get(f"https://pinterest-api-one.vercel.app/?q={query}").json()
 
      media_group = []
      count = 0
@@ -51,3 +51,35 @@ async def pinterest(_, message):
            await msg.delete()
            return await message.reply(f"ᴇʀʀᴏʀ : {e}")
           
+
+
+@app.on_message(filters.command(["chichi"]))
+async def pinterest(_, message):
+    try:
+        query = message.text.split(None, 1)[1]
+    except IndexError:
+        return await message.reply("**ɢɪᴠᴇ ɪᴍᴀɢᴇ ɴᴀᴍᴇ ғᴏʀ sᴇᴀʀᴄʜ 🔍**")
+
+    response = requests.get(f"https://nova-api-seven.vercel.app/api/images?name={query}")
+    image_data = response.json()
+    msg = await message.reply(f"sᴄʀᴀᴘɪɴɢ ɪᴍᴀɢᴇs ғʀᴏᴍ chichi...") 
+    image_urls = image_data.get("image_urls", [])
+
+    images = []
+    max_images = 10
+    for i, url in enumerate(image_urls):
+        if i >= max_images:
+            break
+                
+        image = InputMediaPhoto(url)
+        images.append(image)
+
+    media_groups = [images[i:i + 10] for i in range(0, len(images), 10)]
+
+    for media_group in media_groups:
+         try:
+              await DAXXMUSIC.send_media_group(message.chat.id, media=media_group)
+              return await msg.delete()
+         except Exception as e:
+              await msg.delete()
+              return await message.reply(f"ᴇʀʀᴏʀ : {e}")
