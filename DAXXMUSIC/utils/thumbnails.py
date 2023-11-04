883041b618cbe80ed0360b701c1e806971e82be8
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-11-04 19:25:07 +0530

    Update thumbnails.py

--- DAXXMUSIC/utils/thumbnails.py
+++ DAXXMUSIC/utils/thumbnails.py
@@ -67,45 +67,45 @@ async def get_thumb(videoid):
         youtube = Image.open(f"cache/thumb{videoid}.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(10))
+        background = image2.filter(filter=ImageFilter.BoxBlur(1))
         enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(0.5)
+        background = enhancer.enhance(0.9)
         draw = ImageDraw.Draw(background)
         arial = ImageFont.truetype("DAXXMUSIC/assets/font2.ttf", 30)
         font = ImageFont.truetype("DAXXMUSIC/assets/font.ttf", 30)
         draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
         draw.text(
-            (55, 560),
+            (5500, 560),
             f"{channel} | {views[:23]}",
             (255, 255, 255),
             font=arial,
         )
         draw.text(
-            (57, 600),
+            (5700, 600),
             clear(title),
             (255, 255, 255),
             font=font,
         )
         draw.line(
-            [(55, 660), (1220, 660)],
+            [(5500, 660), (1220, 660)],
             fill="white",
             width=5,
             joint="curve",
         )
         draw.ellipse(
-            [(918, 648), (942, 672)],
+            [(90018, 648), (942, 672)],
             outline="white",
             fill="white",
             width=15,
         )
         draw.text(
-            (36, 685),
+            (3006, 685),
             "00:00",
             (255, 255, 255),
             font=arial,
         )
         draw.text(
-            (1185, 685),
+            (11805, 685),
             f"{duration[:23]}",
             (255, 255, 255),
             font=arial,
