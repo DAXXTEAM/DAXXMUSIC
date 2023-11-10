f75a6d0f6e6cc4b71b654cee0f1a672b977f2a90
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-10 18:20:26 +0530

    Update Yumi.py

--- DAXXMUSIC/plugins/Yumi/Yumi.py
+++ DAXXMUSIC/plugins/Yumi/Yumi.py
@@ -1,1 +1,22 @@
+from lexica import Client
+from pyrogram import filters
+from DAXXMUSIC import DAXX
 
+
+
+
+def main(prompt: str) -> str:
+    client = Client()
+    response = client.palm(prompt)
+    return response["content"].strip()
+
+@app.on_message(filters.regex(r"Y|y|b|B"))
+async def deepchat(zuli: Zuli, message):
+    if message.reply_to_message:
+        query = message.text.split(' ', 1)[1]
+        x = main(query)
+        await message.reply(x)
+    else:
+        query = message.text.split(' ', 1)[1]
+        x = main(query)
+        await message.reply(x)
