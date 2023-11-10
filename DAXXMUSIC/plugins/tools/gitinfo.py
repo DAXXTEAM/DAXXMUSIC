3d26101887b620988c23163d6d88268e6b55cb5f
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-10 15:08:03 +0530

    Update gitinfo.py

--- DAXXMUSIC/plugins/tools/gitinfo.py
+++ DAXXMUSIC/plugins/tools/gitinfo.py
@@ -1,21 +1,24 @@
-import aiohttp
+/eval import aiohttp
 from pyrogram import filters
 from DAXXMUSIC import app
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
         await message.reply_text("/git DAXXTEAM")
         return
+
     username = message.text.split(None, 1)[1]
     URL = f'https://api.github.com/users/{username}'
+
     async with aiohttp.ClientSession() as session:
         async with session.get(URL) as request:
             if request.status == 404:
                 return await message.reply_text("404")
 
             result = await request.json()
+
             try:
                 url = result['html_url']
                 name = result['name']
@@ -28,19 +31,27 @@ async def github(_, message):
                 repositories = result['public_repos']
                 followers = result['followers']
                 following = result['following']
-                caption = f"""ɢɪᴛʜᴜʙ ɪɴғᴏ ᴏғ {name}
 
-ᴜsᴇʀɴᴀᴍᴇ : {username}
-ʙɪᴏ : {bio}
-ʟɪɴᴋ : [Here]({url})
-ᴄᴏᴍᴩᴀɴʏ : {company}
-ᴄʀᴇᴀᴛᴇᴅ ᴏɴ : {created_at}
-ʀᴇᴩᴏsɪᴛᴏʀɪᴇs : {repositories}
-ʙʟᴏɢ : {blog}
-ʟᴏᴄᴀᴛɪᴏɴ : {location}
-ғᴏʟʟᴏᴡᴇʀs : {followers}
-ғᴏʟʟᴏᴡɪɴɢ : {following}"""
+                caption = f"""ɢɪᴛʜᴜʙ ɪɴғᴏ ᴏғ {name}
+                
+ᴜsᴇʀɴᴀᴍᴇ: {username}
+ʙɪᴏ: {bio}
+ʟɪɴᴋ: [Here]({url})
+ᴄᴏᴍᴩᴀɴʏ: {company}
+ᴄʀᴇᴀᴛᴇᴅ ᴏɴ: {created_at}
+ʀᴇᴩᴏsɪᴛᴏʀɪᴇs: {repositories}
+ʙʟᴏɢ: {blog}
+ʟᴏᴄᴀᴛɪᴏɴ: {location}
+ғᴏʟʟᴏᴡᴇʀs: {followers}
+ғᴏʟʟᴏᴡɪɴɢ: {following}"""
+
             except Exception as e:
                 print(str(e))
                 pass
-    await message.reply_photo(photo=avatar_url, caption=caption)
+
+    # Create an inline keyboard with a close button
+    close_button = InlineKeyboardButton("Close", callback_data="close")
+    inline_keyboard = InlineKeyboardMarkup([[close_button]])
+
+    # Send the message with the inline keyboard
+    await message.reply_photo(photo=avatar_url, caption=caption, reply_markup=inline_keyboard)
