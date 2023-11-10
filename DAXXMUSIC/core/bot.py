7d82cb914ba980c3b21e1343bbe95bc812a0d3ba
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2023-11-10 19:52:28 +0530

    Update bot.py

--- DAXXMUSIC/core/bot.py
+++ DAXXMUSIC/core/bot.py
@@ -15,7 +15,6 @@ class DAXX(Client):
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
             in_memory=True,
-            parse_mode=ParseMode.HTML,
             max_concurrent_transmissions=7,
         )
 
