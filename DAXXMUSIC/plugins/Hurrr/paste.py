cd373d0f2fe18b9a300142afc90b6d802265903e
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-10 16:50:29 +0530

    Create paste.py

---
+++ DAXXMUSIC/plugins/Hurrr/paste.py
@@ -0,0 +1,36 @@
+/eval from pyrogram import filters
+from pyrogram.types import Message
+
+from DAXXMUSIC import app
+from DAXXMUSIC.utils.errors import capture_err
+
+@app.on_message(filters.command("paste"))
+@capture_err
+async def paste_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text("Reply To A Message With /paste")
+    m = await message.reply_text("Pasting...")
+    if message.reply_to_message.text:
+        content = str(message.reply_to_message.text)
+    elif message.reply_to_message.document:
+        document = message.reply_to_message.document
+        if document.file_size > 1048576:
+            return await m.edit("You can only paste files smaller than 1MB.")
+        if not pattern.search(document.mime_type):
+            return await m.edit("Only text files can be pasted.")
+        doc = await message.reply_to_message.download()
+        async with aiofiles.open(doc, mode="r") as f:
+            content = await f.read()
+        os.remove(doc)
+    link = await paste(content)
+    preview = link + "/preview.png"
+    button = InlineKeyboard(row_width=1)
+    button.add(InlineKeyboardButton(text="Paste Link", url=link))
+
+    if await isPreviewUp(preview):
+        try:
+            await message.reply_photo(photo=preview, quote=False, reply_markup=button)
+            return await m.delete()
+        except Exception:
+            pass
+    return await m.edit(link)
