d1acf2586e911285814922db88a20a6e78ae88d4
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-19 18:55:17 +0530

    Add files via upload

---
+++ DAXXMUSIC/plugins/tools/carbon.py
@@ -0,0 +1,34 @@
+import aiohttp
+from io import BytesIO
+from DAXXMUSIC import app
+from pyrogram import filters
+from config import COMMAND_HANDLER
+
+
+
+async def make_carbon(code):
+    url = "https://carbonara.solopov.dev/api/cook"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json={"code": code}) as resp:
+            image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
+
+@app.on_message(filters.command("carbon")
+async def _carbon(client, message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴀ ᴄᴀʀʙᴏɴ.**")
+        return
+    if not (replied.text or replied.caption):
+        return await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴀ ᴄᴀʀʙᴏɴ.**")
+    text = await message.reply("Processing...")
+    carbon = await make_carbon(replied.text or replied.caption)
+    await text.edit("**ᴜᴘʟᴏᴀᴅɪɴɢ...**")
+    await message.reply_photo(carbon)
+    await text.delete()
+    carbon.close()
+
+
