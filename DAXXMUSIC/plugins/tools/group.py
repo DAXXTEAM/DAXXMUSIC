7ca265a4afd8af095c12c482bf2b573ce8deb412
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-19 21:34:37 +0530

    Update group.py

--- DAXXMUSIC/plugins/tools/group.py
+++ DAXXMUSIC/plugins/tools/group.py
@@ -26,4 +26,15 @@ async def brah3(app :app, message:Message):
            except:
              pass
 
-##
+
+####
+
+@app.on_message(filters.command("math", prefixes="/"))
+def calculate_math(client, message):   
+    expression = message.text.split("/math ", 1)[1]
+    try:        
+        result = eval(expression)
+        response = f"ᴛʜᴇ ʀᴇsᴜʟᴛ ɪs : {result}"
+    except:
+        response = "ɪɴᴠᴀʟɪᴅ ᴇxᴘʀᴇssɪᴏɴ"
+    message.reply(response)
