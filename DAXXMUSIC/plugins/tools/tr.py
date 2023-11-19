1ac0168cc3e6601149483f55e817fb092b6a92b0
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-19 14:37:38 +0530

    Create tr.py

---
+++ DAXXMUSIC/plugins/tools/tr.py
@@ -0,0 +1,33 @@
+from pyrogram import filters
+from pyrogram.types import *
+from DAXXMUSIC import app
+from gpytranslate import Translator
+ @app.on_message(filters.command(["trans", "tr"]))
+async def translate(_, message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ ɪᴛ !")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = await trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await trans.detect(to_translate)
+        dest = "en"
+    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = (
+        f"ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} to {dest}:\n"
+        f"{translation.text}"
+    )
+    await message.reply_text(reply)
+    
+# --------------------------------------------------------#
