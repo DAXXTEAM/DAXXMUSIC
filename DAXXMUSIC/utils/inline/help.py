130efe46c6cb47a8e880ea3d135ac84cc461393b
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-19 13:46:46 +0530

    Update help.py

--- DAXXMUSIC/utils/inline/help.py
+++ DAXXMUSIC/utils/inline/help.py
@@ -88,7 +88,7 @@ def help_pannel(_, START: Union[bool, int] = None):
             ],
             [ 
                 InlineKeyboardButton(
-                    text=_["H_B_16"],
+                    text=_["chatgpt"],
                     callback_data="help_callback hb16",
                 ),
             ],
