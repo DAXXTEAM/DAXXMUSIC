51791c009ebf2a9371f043843a20dac1258f61e3
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-19 19:30:26 +0530

    Update tringtring.py

--- DAXXMUSIC/plugins/Yumi/tringtring.py
+++ DAXXMUSIC/plugins/Yumi/tringtring.py
@@ -4,7 +4,7 @@ from DAXXMUSIC import app
 from pyrogram import filters
 from pyrogram.types import InputMediaPhoto
 
-@app.on_message(filters.command(["image"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
+@app.on_message(filters.command(["iAmage"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
 async def pinterest(_, message):
      chat_id = message.chat.id
 
