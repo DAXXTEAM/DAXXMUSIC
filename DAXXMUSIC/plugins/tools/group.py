214ab2944e9f8082e1c7a4854bf3a48a125a4f71
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-20 00:45:37 +0530

    Update group.py

--- DAXXMUSIC/plugins/tools/group.py
+++ DAXXMUSIC/plugins/tools/group.py
@@ -47,3 +47,26 @@ async def bot_leave(_, message):
     text = "sᴜᴄᴄᴇssғᴜʟʟʏ   ʟᴇғᴛ ᴛʜᴇ ɢʀᴏᴜᴘ !!."
     await message.reply_text(text)
     await app.leave_chat(chat_id=chat_id, delete=True)
+
+
+####
+
+@app.on_message(filters.command("bots"))
+async def bots(client, message):  
+  try:    
+    botList = []
+    async for bot in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.BOTS):
+      botList.append(bot.user)
+    lenBotList = len(botList) 
+    text3  = f"ʙᴏᴛ ʟɪsᴛ - {message.chat.title}\n\n🤖 ʙᴏᴛs\n"
+    while len(botList) > 1:
+      bot = botList.pop(0)
+      text3 += f"├ @{bot.username}\n"    
+    else:    
+      bot = botList.pop(0)
+      text3 += f"└ @{bot.username}\n\n"
+      text3 += f"✅ | *ᴛᴏᴛᴀʟ ɴᴜᴍʙᴇʀ ᴏғ ʙᴏᴛs**: {lenBotList}"  
+      await app.send_message(message.chat.id, text3)
+  except FloodWait as e:
+    await asyncio.sleep(e.value)
+
