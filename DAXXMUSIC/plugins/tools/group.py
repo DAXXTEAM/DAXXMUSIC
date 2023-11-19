5ecdde24016300b5c8725ab9d254b54301c6e2a4
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-20 00:52:07 +0530

    Update group.py

--- DAXXMUSIC/plugins/tools/group.py
+++ DAXXMUSIC/plugins/tools/group.py
@@ -1,8 +1,5 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram import enums
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import FloodWait
 from DAXXMUSIC import app
 from config import OWNER_ID
 # vc on
@@ -53,23 +50,3 @@ async def bot_leave(_, message):
 
 
 ####
-
-@app.on_message(filters.command("bots"))
-async def bots(client, message):  
-  try:    
-    botList = []
-    async for bot in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.BOTS):
-      botList.append(bot.user)
-    lenBotList = len(botList) 
-    text3  = f"ʙᴏᴛ ʟɪsᴛ - {message.chat.title}\n\n🤖 ʙᴏᴛs\n"
-    while len(botList) > 1:
-      bot = botList.pop(0)
-      text3 += f"├ @{bot.username}\n"    
-    else:    
-      bot = botList.pop(0)
-      text3 += f"└ @{bot.username}\n\n"
-      text3 += f"✅ | *ᴛᴏᴛᴀʟ ɴᴜᴍʙᴇʀ ᴏғ ʙᴏᴛs**: {lenBotList}"  
-      await app.send_message(message.chat.id, text3)
-  except FloodWait as e:
-    await asyncio.sleep(e.value)
-
