8935749d37b1ae860bf49bfc698571bc933687ef
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-19 18:28:35 +0530

    Update group.py

--- DAXXMUSIC/plugins/tools/group.py
+++ DAXXMUSIC/plugins/tools/group.py
@@ -1,6 +1,10 @@
 from pyrogram import Client, filters
 from DAXXMUSIC import app
-
+# vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
        await msg.reply("ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ")
+# vc off
+@app.on_message(filters.video_chat_ended)
+async def brah2(_, msg):
+       await msg.reply("**ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ**")
