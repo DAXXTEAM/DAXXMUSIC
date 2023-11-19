a5163a3dc5f5c28ec659a9087f037599dceda6b2
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-19 13:00:15 +0530

    Update sticker.py

--- DAXXMUSIC/plugins/tools/sticker.py
+++ DAXXMUSIC/plugins/tools/sticker.py
@@ -41,6 +41,62 @@ async def upscale_image(client, message):
         await message.reply_text("**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ**.")
 
 
+######### sticker id
+
+@app.on_message(filters.command("packkang"))
+async def _packkang(app :app,message):  
+    txt = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ....**")
+    if not message.reply_to_message:
+        await txt.edit('ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ')
+        return
+    if not message.reply_to_message.sticker:
+        await txt.edit('ʀᴇᴘʟʏ ᴛᴏ sᴛɪᴄᴋᴇʀ')
+        return
+    if message.reply_to_message.sticker.is_animated or  message.reply_to_message.sticker.is_video:
+        return await txt.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ɴᴏɴ-ᴀɴɪᴍᴀᴛᴇᴅ sᴛɪᴄᴋᴇʀ")
+    if len(message.command) < 2:
+        pack_name =  f'{message.from_user.first_name}_sticker_pack_by_@Yumikoobot'
+    else :
+        pack_name = message.text.split(maxsplit=1)[1]
+    short_name = message.reply_to_message.sticker.set_name
+    stickers = await app.invoke(
+        pyrogram.raw.functions.messages.GetStickerSet(
+            stickerset=pyrogram.raw.types.InputStickerSetShortName(
+                short_name=short_name),
+            hash=0))
+    shits = stickers.documents
+    sticks = []
+    
+    for i in shits:
+        sex = pyrogram.raw.types.InputDocument(
+                id=i.id,
+                access_hash=i.access_hash,
+                file_reference=i.thumbs[0].bytes
+            )
+        
+        sticks.append(
+            pyrogram.raw.types.InputStickerSetItem(
+                document=sex,
+                emoji=i.attributes[1].alt
+            )
+        )
+
+    try:
+        short_name = f'stikcer_pack_{str(uuid4()).replace("-","")}_by_{app.me.username}'
+        user_id = await app.resolve_peer(message.from_user.id)
+        await app.invoke(
+            pyrogram.raw.functions.stickers.CreateStickerSet(
+                user_id=user_id,
+                title=pack_name,
+                short_name=short_name,
+                stickers=sticks,
+            )
+        )
+        await txt.edit(f"**ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴋᴀɴɢᴇᴅ ʟɪɴᴋ**!\n**ᴛᴏᴛᴀʟ sᴛɪᴄᴋᴇʀ **: {len(sticks)}",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴘᴀᴄᴋ ʟɪɴᴋ",url=f"http://t.me/addstickers/{short_name}")]]))
+    except Exception as e:
+        await message.reply(str(e))
+
+
 ###### sticker id =
 @app.on_message(filters.command(["stickerid","stid"]))
 async def sticker_id(app: app, msg):
