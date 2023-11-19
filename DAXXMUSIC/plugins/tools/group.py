2dd1092dc6eb59db0e6872f3d649a66854ded0f8
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-19 18:32:45 +0530

    Update group.py

--- DAXXMUSIC/plugins/tools/group.py
+++ DAXXMUSIC/plugins/tools/group.py
@@ -8,3 +8,19 @@ async def brah(_, msg):
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
        await msg.reply("**ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ**")
+
+# invite members on vc
+@app.on_message(filters.video_chat_members_invited)
+async def fuckoff(app :app, message:Message):
+           text = f"{message.from_user.mention} ɪɴᴠɪᴛᴇᴅ "
+           x = 0
+           for user in message.video_chat_members_invited.users:
+             try:
+               text += f"[{user.first_name}](tg://user?id={user.id}) "
+               x += 1
+             except Exception:
+               pass
+           try:
+             await message.reply(f"{text} 😉")
+           except:
+             pass
