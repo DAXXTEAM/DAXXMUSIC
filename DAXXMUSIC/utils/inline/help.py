7c4693d3d2c37214112766358de2452471f75d21
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-11-19 14:19:33 +0530

    Update help.py

--- DAXXMUSIC/utils/inline/help.py
+++ DAXXMUSIC/utils/inline/help.py
@@ -101,6 +101,8 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_18"],
                     callback_data="help_callback hb18",
                ),
+            ],
+            [
                InlineKeyboardButton(
                     text=_["H_B_19"],
                     callback_data="help_callback hb19",
