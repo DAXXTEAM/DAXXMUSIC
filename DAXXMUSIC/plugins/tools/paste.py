8eb03ff25997a0a534393c499dcdb130d39c00b1
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-20 17:08:34 +0530

    Add files via upload

---
+++ DAXXMUSIC/plugins/tools/paste.py
@@ -0,0 +1,84 @@
+import re, os, socket, aiofiles, aiohttp, asyncio
+from asyncio import get_running_loop
+from functools import partial
+from DAXXMUSIC import app 
+from pyrogram import filters
+from pyrogram.types import *
+from pykeyboard import InlineKeyboard
+from aiohttp import ClientSession
+
+
+aiohttpsession = ClientSession()
+
+
+def _netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096).decode("utf-8").strip("\n\x00")
+        if not data:
+            break
+        return data
+    s.close()
+
+
+async def paste(content):
+    loop = get_running_loop()
+    link = await loop.run_in_executor(None, partial(_netcat, "ezup.dev", 9999, content))
+    return link
+
+
+pattern = re.compile(r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$")
+
+
+
+
+async def isPreviewUp(preview: str) -> bool:
+    for _ in range(7):
+        try:
+            async with aiohttpsession.head(preview, timeout=2) as resp:
+                status = resp.status
+                size = resp.content_length
+        except asyncio.exceptions.TimeoutError:
+            return False
+        if status == 404 or (status == 200 and size == 0):
+            await asyncio.sleep(0.4)
+        else:
+            return status == 200
+    return False
+
+
+@app.on_message(filters.command("paste"))
+async def paste_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ /paste**")
+    m = await message.reply_text("**ᴘᴀsᴛɪɴɢ...**")
+    if message.reply_to_message.text:
+        content = str(message.reply_to_message.text)
+    elif message.reply_to_message.document:
+        document = message.reply_to_message.document
+        if document.file_size > 1048576:
+            return await m.edit("**ʏᴏᴜ ᴄᴀɴ ᴏɴʟʏ ᴘᴀsᴛᴇ ғɪʟᴇs sᴍᴀʟʟᴇʀ ᴛʜᴀɴ 1ᴍʙ.**")
+        if not pattern.search(document.mime_type):
+            return await m.edit("**ᴏɴʟʏ ᴛᴇxᴛ ғɪʟᴇs ᴄᴀɴ ʙᴇ ᴘᴀsᴛᴇᴅ.**")
+        doc = await message.reply_to_message.download()
+        async with aiofiles.open(doc, mode="r") as f:
+            content = await f.read()
+        os.remove(doc)
+    link = await paste(content)
+    preview = f"{link}/preview.png"
+    button = InlineKeyboard(row_width=1)
+    button.add(InlineKeyboardButton(text="ᴘᴀsᴛᴇ ʟɪɴᴋ", url=link))
+
+    if await isPreviewUp(preview):
+        try:
+            await message.reply_photo(photo=preview, quote=False, reply_markup=button)
+            return await m.delete()
+        except Exception:
+            pass
+    return await m.edit(link)
+
+
+
