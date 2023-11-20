bad416661e334c289904eb215545066084e53919
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-20 19:19:53 +0530

    Update tagall.py

--- DAXXMUSIC/plugins/admins/tagall.py
+++ DAXXMUSIC/plugins/admins/tagall.py
@@ -41,7 +41,7 @@ async def tag_all_users(_,message):
                 break 
             usernum += 1
             usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 5:
+            if usernum == 1:
                 await app.send_message(message.chat.id,f'{text}\n{usertxt}')
                 await asyncio.sleep(2)
                 usernum = 0
