d450a61ebe36b26e75935495818d6adf4e3d940e
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-20 13:13:22 +0530

    Update help.py

--- DAXXMUSIC/utils/inline/help.py
+++ DAXXMUSIC/utils/inline/help.py
@@ -117,6 +117,10 @@ def help_pannel(_, START: Union[bool, int] = None):
                 ),
             ],
             [
+                InlineKeyboardButton(
+                    text=_["H_B_22"],
+                    callback_data="help_callback hb22",
+                ),
                
             ],
             mark,
