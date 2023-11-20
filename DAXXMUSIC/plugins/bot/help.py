b3d6f1439fda2a652f461971dbc1d4e9e8bd60de
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-20 11:04:02 +0530

    Update help.py

--- DAXXMUSIC/plugins/bot/help.py
+++ DAXXMUSIC/plugins/bot/help.py
@@ -96,3 +96,8 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_18, reply_markup=keyboard)
     elif cb == "hb19":
         await CallbackQuery.edit_message_text(helpers.HELP_19, reply_markup=keyboard)
+    elif cb == "hb20":
+        await CallbackQuery.edit_message_text(helpers.HELP_20, reply_markup=keyboard)
+    elif cb == "hb21":
+        await CallbackQuery.edit_message_text(helpers.HELP_21, reply_markup=keyboard)
+
