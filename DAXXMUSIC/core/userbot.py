020f52458a74b626a1a34d26dea7146b57b058f7
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2023-11-21 19:36:08 +0530

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -51,8 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.one.join_chat("HEROKUFREECC")
+                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("ALLTYPECC")
             except:
                 pass
             assistants.append(1)
@@ -73,8 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.two.join_chat("HEROKUFREECC")
+                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("ALLTYPECC")
             except:
                 pass
             assistants.append(2)
@@ -94,8 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.three.join_chat("HEROKUFREECC")
+                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("ALLTYPECC")
             except:
                 pass
             assistants.append(3)
@@ -115,8 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.four.join_chat("HEROKUFREECC")
+                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("ALLTYPECC")
             except:
                 pass
             assistants.append(4)
@@ -136,8 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("cyberdaxxx")
-                await self.one.join_chat("cyberdaxx")
+                await self.five.join_chat("HEROKUFREECC")
+                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("ALLTYPECC")
             except:
                 pass
             assistants.append(5)
