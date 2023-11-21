c4fea585d4a3d1367db6fc13a951b9d00e0fad9c
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-21 19:54:37 +0530

    Update song.py

--- DAXXMUSIC/plugins/tools/song.py
+++ DAXXMUSIC/plugins/tools/song.py
@@ -46,4 +46,35 @@ async def song(client: app, message: Message):
     except Exception as e:
         await aux.edit(f"**Error:** {e}")
 
+
+######
+
+
+@app.on_message(command(["ig","instagram","insta"]))
+async def download_instareels(c: app, m: Message):
+    try:
+        reel_ = m.command[1]
+    except IndexError:
+        await m.reply_text("Give me an link to download it...")
+        return
+    if not reel_.startswith("https://www.instagram.com/reel/"):
+        await m.reply_text("In order to obtain the requested reel, a valid link is necessary. Kindly provide me with the required link.")
+        return
+    OwO = reel_.split(".",1)
+    Reel_ = ".dd".join(OwO)
+    try:
+        await m.reply_video(Reel_)
+        return
+    except Exception:
+        try:
+            await m.reply_photo(Reel_)
+            return
+        except Exception:
+            try:
+                await m.reply_document(Reel_)
+                return
+            except Exception:
+                await m.reply_text("I am unable to reach to this reel.")
+
+
   
