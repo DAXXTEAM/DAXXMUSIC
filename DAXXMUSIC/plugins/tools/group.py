fa6b9a85ed94e2cd867d4912214432c52c371623
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-21 23:47:10 +0530

    Update group.py

--- DAXXMUSIC/plugins/tools/group.py
+++ DAXXMUSIC/plugins/tools/group.py
@@ -50,3 +50,32 @@ async def bot_leave(_, message):
 
 
 ####
+
+
+@app.on_message(filters.command(["spg"], ["/", "!", "."]))
+async def search(event):
+    msg = await event.respond("Searching...")
+    async with aiohttp.ClientSession() as session:
+        start = 1
+        async with session.get(f"https://content-customsearch.googleapis.com/customsearch/v1?cx=ec8db9e1f9e41e65e&q={event.text.split()[1]}&key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM&start={start}", headers={"x-referer": "https://explorer.apis.google.com"}) as r:
+            response = await r.json()
+            result = ""
+            
+            if not response.get("items"):
+                return await msg.edit("No results found!")
+            for item in response["items"]:
+                title = item["title"]
+                link = item["link"]
+                if "/s" in item["link"]:
+                    link = item["link"].replace("/s", "")
+                elif re.search(r'\/\d', item["link"]):
+                    link = re.sub(r'\/\d', "", item["link"])
+                if "?" in link:
+                    link = link.split("?")[0]
+                if link in result:
+                    # remove duplicates
+                    continue
+                result += f"{title}\n{link}\n\n"
+            prev_and_next_btns = [Button.inline("▶️Next▶️", data=f"next {start+10} {event.text.split()[1]}")]
+            await msg.edit(result, link_preview=False, buttons=prev_and_next_btns)
+            await session.close()
