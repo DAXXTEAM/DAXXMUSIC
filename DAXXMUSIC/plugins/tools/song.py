9857135dd7d398aa8010b1fe551ed1728941fd2a
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-21 20:14:21 +0530

    Update song.py

--- DAXXMUSIC/plugins/tools/song.py
+++ DAXXMUSIC/plugins/tools/song.py
@@ -75,35 +75,3 @@ async def download_instareels(c: app, m: Message):
                 return
             except Exception:
                 await m.reply_text("I am unable to reach to this reel.")
-
-
-#### YOUTUBE DOWNLOAD
-
-@app.on_message(filters.command(["yt"], ["/", "!", "."]))
-async def download_instareels(c: app, m: Message):
-    try:
-        reel_ = m.command[1]
-    except IndexError:
-        await m.reply_text("Give me an link to download it...")
-        return
-    if not reel_.startswith("https://youtube.com/shorts/"):
-        await m.reply_text("In order to obtain the requested , a valid link is necessary. Kindly provide me with the required link.")
-        return
-    OwO = reel_.split(".",1)
-    Reel_ = ".dd".join(OwO)
-    try:
-        await m.reply_video(Reel_)
-        return
-    except Exception:
-        try:
-            await m.reply_photo(Reel_)
-            return
-        except Exception:
-            try:
-                await m.reply_document(Reel_)
-                return
-            except Exception:
-                await m.reply_text("I am unable to reach to this ")
-
-
-  
