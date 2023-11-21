71fd758c149f52dd71e3f88ae51937c2bbe3f1bb
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-21 21:03:57 +0530

    Add files via upload

---
+++ DAXXMUSIC/plugins/Yumi/dicegame.py
@@ -0,0 +1,51 @@
+from pyrogram import Client, enums, filters
+#from config import *
+import asyncio
+from DAXXMUSIC import app as app
+
+from pyrogram.handlers import MessageHandler
+
+
+@app.on_message(filters.command("dice"))
+async def dice(bot, message):
+    x=await bot.send_dice(message.chat.id)
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+  
+@app.on_message(filters.command("dart"))
+async def dart(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎯")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+
+@app.on_message(filters.command("basket"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🏀")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+@app.on_message(filters.command("jackpot"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎰")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+@app.on_message(filters.command("ball"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎳")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+@app.on_message(filters.command("football"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "⚽")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+__help__ = """
+ Play Game With Emojis:
+/dice - Dice 🎲
+/dart - Dart 🎯
+/basket - Basket Ball 🏀
+/ball - Bowling Ball 🎳
+/football - Football ⚽
+/jackpot - Spin slot machine 🎰
+ """
+
+__mod_name__ = "Dɪᴄᴇ"
