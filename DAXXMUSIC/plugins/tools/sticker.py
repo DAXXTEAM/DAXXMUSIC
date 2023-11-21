071b249685853322396137feeefecd6e34f7d962
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-21 10:17:30 +0530

    Update sticker.py

--- DAXXMUSIC/plugins/tools/sticker.py
+++ DAXXMUSIC/plugins/tools/sticker.py
@@ -112,19 +112,4 @@ async def sticker_id(app: app, msg):
 """)
 
 
-######
-@app.on_message(filters.command("up"))
-async def upscaleImages(_, message):
-    file = await getFile(message)
-    if file is None:
-        return await message.reply_text("Replay to an image?")
-    msg = await message.reply("Wait A Min.. Upscalling Your Image")
-    imageBytes = open(file,"rb").read()
-    os.remove(file)
-    upscaledImage = await UpscaleImages(imageBytes)
-    try:
-      await message.reply_document(open(upscaledImage,"rb"), caption=f"Upscaled By @{BOT_USERNAME}")
-      await msg.delete()
-      os.remove(upscaledImage)
-    except Exception as e:
-       await msg.edit(f"{e}")
+#####
