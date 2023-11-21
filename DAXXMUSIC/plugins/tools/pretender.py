ef800c703507c0f271bc1a60bcc654d5ef3d52f1
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-21 20:46:41 +0530

    Update pretender.py

--- DAXXMUSIC/plugins/tools/pretender.py
+++ DAXXMUSIC/plugins/tools/pretender.py
@@ -30,6 +30,38 @@ async def chk_usr(_, message: Message):
 **🍊 ɴᴀᴍᴇ** : {message.from_user.mention}
 **🍅 ᴜsᴇʀ ɪᴅ** : {message.from_user.id}
 ➖➖➖➖➖➖➖➖➖➖➖➖\n
+from pyrogram import filters
+from pyrogram.types import Message
+from DAXXMUSIC.plugins.tools.pretenderdb import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
+from DAXXMUSIC import app
+
+
+
+
+@app.on_message(filters.group & ~filters.bot & ~filters.via_bot, group=69)
+async def chk_usr(_, message: Message):
+    if message.sender_chat or not await check_pretender(message.chat.id):
+        return
+    if not await usr_data(message.from_user.id):
+        return await add_userdata(
+            message.from_user.id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
+        )
+    usernamebefore, first_name, lastname_before = await get_userdata(message.from_user.id)
+    msg = ""
+    if (
+        usernamebefore != message.from_user.username
+        or first_name != message.from_user.first_name
+        or lastname_before != message.from_user.last_name
+    ):
+        msg += f"""
+**🔓 ᴘʀᴇᴛᴇɴᴅᴇʀ ᴅᴇᴛᴇᴄᴛᴇᴅ 🔓**
+➖➖➖➖➖➖➖➖➖➖➖➖
+**🍊 ɴᴀᴍᴇ** : {message.from_user.mention}
+**🍅 ᴜsᴇʀ ɪᴅ** : {message.from_user.id}
+➖➖➖➖➖➖➖➖➖➖➖➖\n
 """
     if usernamebefore != message.from_user.username:
         usernamebefore = f"@{usernamebefore}" if usernamebefore else "NO USERNAME"
@@ -93,14 +125,14 @@ async def chk_usr(_, message: Message):
 async def set_mataa(_, message: Message):
     if len(message.command) == 1:
         return await message.reply("**ᴅᴇᴛᴇᴄᴛ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴜsᴇʀs ᴜsᴀɢᴇ : ᴘʀᴇᴛᴇɴᴅᴇʀ ᴏɴ|ᴏғғ**")
-    if message.command[1] == "on":
+    if message.command[1] == "enable":
         cekset = await impo_on(message.chat.id)
         if cekset:
             await message.reply("**ᴘʀᴇᴛᴇɴᴅᴇʀ ᴍᴏᴅᴇ ɪs ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ.**")
         else:
             await impo_on(message.chat.id)
             await message.reply(f"**sᴜᴄᴄᴇssғᴜʟʟʏ ᴇɴᴀʙʟᴇᴅ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴍᴏᴅᴇ ғᴏʀ** {message.chat.title}")
-    elif message.command[1] == "off":
+    elif message.command[1] == "disable":
         cekset = await impo_off(message.chat.id)
         if not cekset:
             await message.reply("**ᴘʀᴇᴛᴇɴᴅᴇʀ ᴍᴏᴅᴇ ɪs ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ.**")
@@ -108,4 +140,55 @@ async def set_mataa(_, message: Message):
             await impo_off(message.chat.id)
             await message.reply(f"**sᴜᴄᴄᴇssғᴜʟʟʏ ᴅɪsᴀʙʟᴇᴅ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴍᴏᴅᴇ ғᴏʀ** {message.chat.title}")
     else:
-        await message.reply("**ᴅᴇᴛᴇᴄᴛ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴜsᴇʀs ᴜsᴀɢᴇ : ᴘʀᴇᴛᴇɴᴅᴇʀ ᴏɴ|ᴏғғ**")
+        await message.reply("**ᴅᴇᴛᴇᴄᴛ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴜsᴇʀs ᴜsᴀɢᴇ : ᴘʀᴇᴛᴇɴᴅᴇʀ ᴏɴ|ᴏғғ**")"""
+    if usernamebefore != message.from_user.username:
+        usernamebefore = f"@{usernamebefore}" if usernamebefore else "NO USERNAME"
+        usernameafter = (
+            f"@{message.from_user.username}"
+            if message.from_user.username
+            else "NO USERNAME"
+        )
+        msg += """
+**🐻‍❄️ ᴄʜᴀɴɢᴇᴅ ᴜsᴇʀɴᴀᴍᴇ 🐻‍❄️**
+➖➖➖➖➖➖➖➖➖➖➖➖
+**🎭 ғʀᴏᴍ** : {bef}
+**🍜 ᴛᴏ** : {aft}
+➖➖➖➖➖➖➖➖➖➖➖➖\n
+""".format(bef=usernamebefore, aft=usernameafter)
+        await add_userdata(
+            message.from_user.id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
+        )
+    if first_name != message.from_user.first_name:
+        msg += """
+**🪧 ᴄʜᴀɴɢᴇs ғɪʀsᴛ ɴᴀᴍᴇ 🪧**
+➖➖➖➖➖➖➖➖➖➖➖➖
+**🔐 ғʀᴏᴍ** : {bef}
+ᴏ** : {aft}
+➖➖➖➖➖➖➖➖➖➖➖➖\n
+""".format(
+            bef=first_name, aft=message.from_user.first_name
+        )
+        await add_userdata(
+            message.from_user.id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
+        )
+    if lastname_before != message.from_user.last_name:
+        lastname_before = lastname_before or "NO LAST NAME"
+        lastname_after = message.from_user.last_name or "NO LAST NAME"
+        msg += """
+**🪧 ᴄʜᴀɴɢᴇs ʟᴀsᴛ ɴᴀᴍᴇ 🪧**
+➖➖➖➖➖➖➖➖➖➖➖➖
+**🚏ғʀᴏᴍ** : {bef}
+**🍕 ᴛᴏ** : {aft}
+➖➖➖➖➖➖➖➖➖➖➖➖\n
+""".format(
+            bef=lastname_before, aft=lastname_after
+        )
+        await add_userdata(
+            message.from_user.id,
+            message.from_user.username,
