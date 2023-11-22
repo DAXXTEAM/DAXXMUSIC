c581d8769c127a8530fc23dbb2c8c12d877ef47d
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-11-22 19:02:50 +0530

    Create mongo.py.txt

---
+++ DAXXMUSIC/utils/mongo.py
@@ -0,0 +1,44 @@
+from typing import Dict, Union
+
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+
+from DAXXMUSIC import MONGO_DB_URI
+
+mongo = MongoCli(MONGO_DB_URI)
+db = mongo.DAXXMUSIC
+
+coupledb = db.couple
+
+
+async def _get_lovers(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
+        lovers = lovers["couple"]
+    else:
+        lovers = {}
+    return lovers
+
+async def _get_image(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
+        lovers = lovers["img"]
+    else:
+        lovers = {}
+    return lovers
+
+async def get_couple(cid: int, date: str):
+    lovers = await _get_lovers(cid)
+    if date in lovers:
+        return lovers[date]
+    else:
+        return False
+
+
+async def save_couple(cid: int, date: str, couple: dict, img: str):
+    lovers = await _get_lovers(cid)
+    lovers[date] = couple
+    await coupledb.update_one(
+        {"chat_id": cid},
+        {"$set": {"couple": lovers, "img": img}},
+        upsert=True,
+    )
