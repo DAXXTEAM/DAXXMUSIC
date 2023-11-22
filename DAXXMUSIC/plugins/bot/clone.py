fa431c3c56223e58078ce8e5edeef396c3b55d73
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-22 08:21:48 +0530

    Update clone.py

--- DAXXMUSIC/plugins/bot/clone.py
+++ DAXXMUSIC/plugins/bot/clone.py
@@ -11,7 +11,7 @@ from config import API_ID, API_HASH
 IMG = ["https://telegra.ph/file/1cefd3211a5acdcd332415.jpg", "https://telegra.ph/file/130d743cea510c563af6e3.jpg", "https://telegra.ph/file/1f7ae22a1491f530c05279.jpg", "https://telegra.ph/file/12f1c9c98452ae9a958f7d.jpg"]
 MESSAGE = "Heya! I'm a music bot hoster/Cloner\n\nI can Host Your Bot On My Server within seconds\n\nTry /clone Token from @botfather"
 
-@app.on_message(filters.private & filters.command("start"))
+@app.on_message(filters.private & filters.command("copy"))
 async def hello(client, message: Message):
     buttons = [
            [
