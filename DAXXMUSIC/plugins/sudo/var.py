14abdf204acc64552d2c82f644ab6064f6767adf
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-22 06:31:29 +0530

    Update var.py

--- DAXXMUSIC/plugins/sudo/var.py
+++ DAXXMUSIC/plugins/sudo/var.py
@@ -24,11 +24,6 @@ async def varsFunc(client, message):
  
     text = f"""MUSIC BOT CONFIG:
 
-<u>Basic Vars:</u>
-MUSIC_BOT_NAME : {bot_name}
-DURATION_LIMIT : {play_duration} min
-SONG_DOWNLOAD_DURATION_LIMIT : {song} min
-OWNER_ID : {owner_id}
     
 <u>Custom Repo Vars:</u>
 UPSTREAM_REPO : {up_r}
@@ -37,32 +32,6 @@ GITHUB_REPO : {git}
 GIT_TOKEN : {token}
 
 
-<u>Bot Vars:</u>
-AUTO_LEAVING_ASSISTANT : {ass}
-ASSISTANT_LEAVE_TIME : {auto_leave} seconds
-AUTO_SUGGESTION_MODE : {a_sug}
-AUTO_SUGGESTION_TIME : {auto_sug} seconds
-AUTO_DOWNLOADS_CLEAR : {down}
-PRIVATE_BOT_MODE : {pvt}
-YOUTUBE_EDIT_SLEEP : {yt_sleep} seconds
-TELEGRAM_EDIT_SLEEP : {tg_sleep} seconds
-CLEANMODE_MINS : {cm} mins
-VIDEO_STREAM_LIMIT : {v_limit} chats
-SERVER_PLAYLIST_LIMIT : {playlist_limit}
-PLAYLIST_FETCH_LIMIT : {fetch_playlist}
-
-<u>Spotify Vars:</u>
-SPOTIFY_CLIENT_ID : {sotify}
-SPOTIFY_CLIENT_SECRET : {sotify}
-
-<u>Playsize Vars:</u>
-TG_AUDIO_FILESIZE_LIMIT : {tg_aud}
-TG_VIDEO_FILESIZE_LIMIT : {tg_vid}
-
-<u>URL Vars:</u>
-SUPPORT_CHANNEL : {s_c}
-SUPPORT_GROUP :  {s_g}
-START_IMG_URL :  {start}
     """
     await asyncio.sleep(1)
     await mystic.edit_text(text)
