fbfe7cdd70d2e2632d3f38e1c498806883f29dd1
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-11-22 19:36:48 +0530

    Update mongo.py

--- DAXXMUSIC/utils/mongo.py
+++ DAXXMUSIC/utils/mongo.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from DAXXMUSIC import MONGO_DB_URI
+from DAXXMUSIC import config.MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.DAXXMUSIC
