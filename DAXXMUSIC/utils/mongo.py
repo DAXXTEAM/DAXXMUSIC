ee6c24c8b8a98a8b8b9d8ca451fdec2e94900eca
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-22 19:56:50 +0530

    Update mongo.py

--- DAXXMUSIC/utils/mongo.py
+++ DAXXMUSIC/utils/mongo.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from DAXXMUSIC import MONGO_DB_URI
+from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.DAXXMUSIC
