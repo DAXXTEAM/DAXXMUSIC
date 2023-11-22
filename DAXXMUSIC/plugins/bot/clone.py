f94f23362e490eef3164a72babb4be171fa5b58a
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-22 08:36:36 +0530

    Update clone.py

--- DAXXMUSIC/plugins/bot/clone.py
+++ DAXXMUSIC/plugins/bot/clone.py
@@ -6,7 +6,7 @@ from pyrogram import *
 from pyrogram.types import *
 from random import choice
 from DAXXMUSIC import app
-from config import API_ID, API_HASH
+from config import API_ID, API_HASH, ASSUSERNAME
 
 IMG = ["https://telegra.ph/file/1cefd3211a5acdcd332415.jpg", "https://telegra.ph/file/130d743cea510c563af6e3.jpg", "https://telegra.ph/file/1f7ae22a1491f530c05279.jpg", "https://telegra.ph/file/12f1c9c98452ae9a958f7d.jpg"]
 MESSAGE = "Heya! I'm a music bot hoster/Cloner\n\nI can Host Your Bot On My Server within seconds\n\nTry /clone Token from @botfather"
