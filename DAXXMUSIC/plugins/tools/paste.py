3f7e7771ab268e5dc2cfc07dd2247ad9a7124da3
LoveFeelingsWill <147972786+LoveFeelingsWill@users.noreply.github.com>
2023-11-22 15:34:50 +0530

    Update paste.py

--- DAXXMUSIC/plugins/tools/paste.py
+++ DAXXMUSIC/plugins/tools/paste.py
@@ -1,15 +1,19 @@
-import re, os, socket, aiofiles, aiohttp, asyncio
-from asyncio import get_running_loop
+from asyncio import get_running_loop, sleep, TimeoutError
 from functools import partial
-from DAXXMUSIC import app 
+from DAXXMUSIC import app
 from pyrogram import filters
-from pyrogram.types import *
-from pykeyboard import InlineKeyboard
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiohttp import ClientSession
-
+import re
+import os
+import socket
+import aiofiles
+import aiohttp
+import asyncio
 
 aiohttpsession = ClientSession()
 
+# Rest of your code remains unchanged...
 
 def _netcat(host, port, content):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,17 +27,12 @@ def _netcat(host, port, content):
         return data
     s.close()
 
-
 async def paste(content):
     loop = get_running_loop()
     link = await loop.run_in_executor(None, partial(_netcat, "ezup.dev", 9999, content))
     return link
 
-
 pattern = re.compile(r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$")
-
-
-
 
 async def isPreviewUp(preview: str) -> bool:
     for _ in range(7):
@@ -41,7 +40,7 @@ async def isPreviewUp(preview: str) -> bool:
             async with aiohttpsession.head(preview, timeout=2) as resp:
                 status = resp.status
                 size = resp.content_length
-        except asyncio.exceptions.TimeoutError:
+        except asyncio.TimeoutError:
             return False
         if status == 404 or (status == 200 and size == 0):
             await asyncio.sleep(0.4)
@@ -49,20 +48,19 @@ async def isPreviewUp(preview: str) -> bool:
             return status == 200
     return False
 
-
 @app.on_message(filters.command("paste"))
 async def paste_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ /paste**")
-    m = await message.reply_text("**ᴘᴀsᴛɪɴɢ...**")
+        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ /paste")
+    m = await message.reply_text("ᴘᴀsᴛɪɴɢ...")
     if message.reply_to_message.text:
         content = str(message.reply_to_message.text)
     elif message.reply_to_message.document:
         document = message.reply_to_message.document
         if document.file_size > 1048576:
-            return await m.edit("**ʏᴏᴜ ᴄᴀɴ ᴏɴʟʏ ᴘᴀsᴛᴇ ғɪʟᴇs sᴍᴀʟʟᴇʀ ᴛʜᴀɴ 1ᴍʙ.**")
+            return await m.edit("ʏᴏᴜ ᴄᴀɴ ᴏɴʟʏ ᴘᴀsᴛᴇ ғɪʟᴇs sᴍᴀʟʟᴇʀ ᴛʜᴀɴ 1ᴍʙ.")
         if not pattern.search(document.mime_type):
-            return await m.edit("**ᴏɴʟʏ ᴛᴇxᴛ ғɪʟᴇs ᴄᴀɴ ʙᴇ ᴘᴀsᴛᴇᴅ.**")
+            return await m.edit("ᴏɴʟʏ ᴛᴇxᴛ ғɪʟᴇs ᴄᴀɴ ʙᴇ ᴘᴀsᴛᴇᴅ.")
         doc = await message.reply_to_message.download()
         async with aiofiles.open(doc, mode="r") as f:
             content = await f.read()
