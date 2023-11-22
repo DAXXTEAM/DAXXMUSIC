bfa7fab6a80c779c19a8bf065990779ec0304d63
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-22 06:30:03 +0530

    Update var.py

--- DAXXMUSIC/plugins/sudo/var.py
+++ DAXXMUSIC/plugins/sudo/var.py
@@ -19,54 +19,9 @@ async def varsFunc(client, message):
     )
     up_r = f"[Repo]({config.UPSTREAM_REPO})"
     up_b = config.UPSTREAM_BRANCH
-    
-        ass = "Yes"
-    else:
-        ass = "No"
-    if config.PRIVATE_BOT_MODE == str(True):
-        pvt = "Yes"
-    else:
-        pvt = "No"
-    if config.AUTO_SUGGESTION_MODE == str(True):
-        a_sug = "Yes"
-    else:
-        a_sug = "No"
-    if config.AUTO_DOWNLOADS_CLEAR == str(True):
-        down = "Yes"
-    else:
-        down = "No"
 
-    if not config.GITHUB_REPO:
-        git = "No"
-    else:
-        git = f"[Repo]({config.GITHUB_REPO})"
-    if not config.START_IMG_URL:
-        start = "No"
-    else:
-        start = f"[Image]({config.START_IMG_URL})"
-    if not config.SUPPORT_CHANNEL:
-        s_c = "No"
-    else:
-        s_c = f"[Channel]({config.SUPPORT_CHANNEL})"
-    if not config.SUPPORT_GROUP:
-        s_g = "No"
-    else:
-        s_g = f"[Group]({config.SUPPORT_GROUP})"
-    if not config.GIT_TOKEN:
-        token = "No"
-    else:
-        token = "Yes"
-    if (
-        not config.SPOTIFY_CLIENT_ID
-        and not config.SPOTIFY_CLIENT_SECRET
-    ):
-        sotify = "No"
-    else:
-        sotify = "Yes"
-    owners = [str(ids) for ids in config.OWNER_ID]
-    owner_id = " ,".join(owners)
-    tg_aud = convert_bytes(config.TG_AUDIO_FILESIZE_LIMIT)
-    tg_vid = convert_bytes(config.TG_VIDEO_FILESIZE_LIMIT)
+ ##############
+ 
     text = f"""MUSIC BOT CONFIG:
 
 <u>Basic Vars:</u>
