93ff4e660fe3148b8f223cb4476346dae9971117
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-22 08:30:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,9 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
+#get Your Assistant User name
+ASSUSERNAME = getevn("ASSUSERNAME")
+
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
