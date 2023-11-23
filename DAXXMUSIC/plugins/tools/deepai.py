d22f727b81b7711c81b7fbf61530d8b940d905d2
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-23 17:29:26 +0530

    Update deepai.py

--- DAXXMUSIC/plugins/tools/deepai.py
+++ DAXXMUSIC/plugins/tools/deepai.py
@@ -35,7 +35,7 @@ async def deepchat(app: app, message):
 
 #####
 
-@app.on_message(filters.command(["aby" , ],  prefixes=["b","Y"]))
+@app.on_message(filters.command(["aby" , ],  prefixes=["b","B"]))
 async def deepchat(app: app, message):
     name = message.from_user.first_name
     try:
