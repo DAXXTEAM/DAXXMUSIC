d379f0a855f42ec119c7833cf5463e0da883a531
LoveFeelingsWill <147972786+LoveFeelingsWill@users.noreply.github.com>
2023-11-23 14:54:22 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -46,7 +46,6 @@ async def start_pm(client, message: Message, _):
             keyboard = help_pannel(_)
             return await message.reply_photo(
                 random.choice(YUMI_PICS),
-                protect_content=True,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -99,7 +98,6 @@ async def start_pm(client, message: Message, _):
         out = private_panel(_)
         await message.reply_photo(
             random.choice(YUMI_PICS),
-            protect_content=True,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -117,7 +115,6 @@ async def start_gp(client, message: Message, _):
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
         random.choice(YUMI_PICS),
-        protect_content=True,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -153,7 +150,6 @@ async def welcome(client, message: Message):
                 out = start_panel(_)
                 await message.reply_photo(
                     random.choice(YUMI_PICS),
-                    protect_content=True,
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
