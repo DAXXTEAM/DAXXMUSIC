e2bd47d3f228524a137e958c28330ebe0364c83e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-23 14:10:54 +0530

    Update botschk.py

--- DAXXMUSIC/plugins/sudo/botschk.py
+++ DAXXMUSIC/plugins/sudo/botschk.py
@@ -13,7 +13,7 @@ BOT_LIST = ["YumikooBot", "DAXXTEAMBOT"]
 
 
 
-@app.on_message(filters.command("botschk") & filters.user(SUDOERS))
+@app.on_message(filters.command("botschk") & SUDOERS)
 async def bots_chk(celestia, message):
     msg = await message.reply_photo(photo="https://telegra.ph/file/48578068b7574bb25a529.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
     response = "**ʙᴏᴛs sᴛᴀᴛᴜs ᴅᴇᴀᴅ ᴏʀ ᴀʟɪᴠᴇ ᴄʜᴇᴄᴋᴇʀ**\n\n"
