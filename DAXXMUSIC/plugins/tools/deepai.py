20bea86b34c39f83d1884423a752276e8af01dd4
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-23 17:26:16 +0530

    Update deepai.py

--- DAXXMUSIC/plugins/tools/deepai.py
+++ DAXXMUSIC/plugins/tools/deepai.py
@@ -34,3 +34,31 @@ async def deepchat(app: app, message):
 
 
 #####
+
+@app.on_message(filters.command(["aby" , ],  prefixes=["b","Y"]))
+async def deepchat(app: app, message):
+    name = message.from_user.first_name
+    try:
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(f"Hello {name}\nPlease provide text after the /deep command.")
+        else:
+            a = message.text.split(' ', 1)[1]
+
+            data = {
+                'text': a,  
+            }
+
+            headers = {
+                'api-key': api_key,
+            }
+
+            r = requests.post("https://api.deepai.org/api/text-generator", data=data, headers=headers)
+            response = r.json()
+            answer_text = response['output']
+            await message.reply_text(f"{answer_text}")
+    except Exception as e:
+        await message.reply_text(f"ᴇʀʀᴏʀ: {e}")
+
+
+##
