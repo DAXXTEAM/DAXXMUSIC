82fb979166c9857b1919a2f41827dd427e33a0f3
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-24 00:58:08 +0530

    Add files via upload

---
+++ DAXXMUSIC/plugins/tools/afk.py
@@ -0,0 +1,181 @@
+import time
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from DAXXMUSIC import app
+from DAXXMUSIC.mongo.readable_time import get_readable_time
+from DAXXMUSIC.mongo.afkdb import add_afk, is_afk, remove_afk
+
+
+@app.on_message(filters.command(["afk", "brb"]))
+async def active_afk(_, message: Message):
+    if message.sender_chat:
+        return
+    user_id = message.from_user.id
+    verifier, reasondb = await is_afk(user_id)
+    if verifier:
+        await remove_afk(user_id)
+        try:
+            afktype = reasondb["type"]
+            timeafk = reasondb["time"]
+            data = reasondb["data"]
+            reasonafk = reasondb["reason"]
+            seenago = get_readable_time((int(time.time() - timeafk)))
+            if afktype == "text":
+                send = await message.reply_text(
+                    f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}",
+                    disable_web_page_preview=True,
+                )
+            if afktype == "text_reason":
+                send = await message.reply_text(
+                    f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`",
+                    disable_web_page_preview=True,
+                )
+            if afktype == "animation":
+                if str(reasonafk) == "None":
+                    send = await message.reply_animation(
+                        data,
+                        caption=f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}",
+                    )
+                else:
+                    send = await message.reply_animation(
+                        data,
+                        caption=f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`",
+                    )
+            if afktype == "photo":
+                if str(reasonafk) == "None":
+                    send = await message.reply_photo(
+                        photo=f"downloads/{user_id}.jpg",
+                        caption=f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}",
+                    )
+                else:
+                    send = await message.reply_photo(
+                        photo=f"downloads/{user_id}.jpg",
+                        caption=f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`",
+                    )
+        except Exception:
+            send = await message.reply_text(
+                f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ",
+                disable_web_page_preview=True,
+            )
+
+    if len(message.command) == 1 and not message.reply_to_message:
+        details = {
+            "type": "text",
+            "time": time.time(),
+            "data": None,
+            "reason": None,
+        }
+    elif len(message.command) > 1 and not message.reply_to_message:
+        _reason = (message.text.split(None, 1)[1].strip())[:100]
+        details = {
+            "type": "text_reason",
+            "time": time.time(),
+            "data": None,
+            "reason": _reason,
+        }
+    elif len(message.command) == 1 and message.reply_to_message.animation:
+        _data = message.reply_to_message.animation.file_id
+        details = {
+            "type": "animation",
+            "time": time.time(),
+            "data": _data,
+            "reason": None,
+        }
+    elif len(message.command) > 1 and message.reply_to_message.animation:
+        _data = message.reply_to_message.animation.file_id
+        _reason = (message.text.split(None, 1)[1].strip())[:100]
+        details = {
+            "type": "animation",
+            "time": time.time(),
+            "data": _data,
+            "reason": _reason,
+        }
+    elif len(message.command) == 1 and message.reply_to_message.photo:
+        await app.download_media(
+            message.reply_to_message, file_name=f"{user_id}.jpg"
+        )
+        details = {
+            "type": "photo",
+            "time": time.time(),
+            "data": None,
+            "reason": None,
+        }
+    elif len(message.command) > 1 and message.reply_to_message.photo:
+        await app.download_media(
+            message.reply_to_message, file_name=f"{user_id}.jpg"
+        )
+        _reason = message.text.split(None, 1)[1].strip()
+        details = {
+            "type": "photo",
+            "time": time.time(),
+            "data": None,
+            "reason": _reason,
+        }
+    elif len(message.command) == 1 and message.reply_to_message.sticker:
+        if message.reply_to_message.sticker.is_animated:
+            details = {
+                "type": "text",
+                "time": time.time(),
+                "data": None,
+                "reason": None,
+            }
+        else:
+            await app.download_media(
+                message.reply_to_message, file_name=f"{user_id}.jpg"
+            )
+            details = {
+                "type": "photo",
+                "time": time.time(),
+                "data": None,
+                "reason": None,
+            }
+    elif len(message.command) > 1 and message.reply_to_message.sticker:
+        _reason = (message.text.split(None, 1)[1].strip())[:100]
+        if message.reply_to_message.sticker.is_animated:
+            details = {
+                "type": "text_reason",
+                "time": time.time(),
+                "data": None,
+                "reason": _reason,
+            }
+        else:
+            await app.download_media(
+                message.reply_to_message, file_name=f"{user_id}.jpg"
+            )
+            details = {
+                "type": "photo",
+                "time": time.time(),
+                "data": None,
+                "reason": _reason,
+            }
+    else:
+        details = {
+            "type": "text",
+            "time": time.time(),
+            "data": None,
+            "reason": None,
+        }
+
+    await add_afk(user_id, details)
+    await message.reply_sticker(
+        "CAACAgUAAx0CUgguZAABAdegY2N5paaiPapUxRm0RYy9Xf6dPEYAAisIAAJ2PRlXxkn7UgOIdewqBA"
+    )
+    await message.reply_text(f"{message.from_user.first_name} ɪs ɴᴏᴡ ᴀғᴋ!")
+
+
+__mod_name__ = "𝐀ғᴋ"
+
+
+# ғᴏʀ ʜᴇʟᴘ ᴍᴇɴᴜ
+
+# """
+from DAXXMUSIC.modules.language import gs
+
+
+def get_help(chat):
+    return gs(chat, "afk_help")
+
+
+# """
