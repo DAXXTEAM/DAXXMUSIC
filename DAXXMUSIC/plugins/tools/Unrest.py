51c33fc9abee2ff63a5bb2699bde464e10469e36
LoveFeelingsWill <147972786+LoveFeelingsWill@users.noreply.github.com>
2023-11-24 10:44:54 +0530

    Update Unrest.py

--- DAXXMUSIC/plugins/tools/Unrest.py
+++ DAXXMUSIC/plugins/tools/Unrest.py
@@ -23,7 +23,7 @@ async def upscale_image(client, message):
 
         async with httpx.AsyncClient() as http_client:
             response = await http_client.post(
-                "https://clipdrop-api.co/image-upscaling/v1/upscale",
+                "https://super-image1.p.rapidapi.com/v2/run",
                 data={"image_data": b},
                 headers=headers,
                 timeout=30,  # Adjust the timeout as needed
