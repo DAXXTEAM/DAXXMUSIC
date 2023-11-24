bfae41f6112736d49a7a1e532e0fc34ab0303cb3
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-24 15:28:08 +0530

    Update couples.py

--- DAXXMUSIC/plugins/tools/couples.py
+++ DAXXMUSIC/plugins/tools/couples.py
@@ -90,8 +90,8 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (300, 300), img1)
-         img.paste(img2, (1500, 300), img2)
+         img.paste(img1, (400, 300), img1)
+         img.paste(img2, (700, 300), img2)
 
          img.save(f'test_{cid}.png')
     
