f46d195389bfe72d4c31ed96652323624369433c
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-25 09:43:46 +0530

    Update group.py

--- DAXXMUSIC/plugins/tools/group.py
+++ DAXXMUSIC/plugins/tools/group.py
@@ -44,7 +44,7 @@ def calculate_math(client, message):
 @app.on_message(filters.command("leavegroup")& filters.user(OWNER_ID))
 async def bot_leave(_, message):
     chat_id = message.chat.id
-    text = "sᴜᴄᴄᴇssғᴜʟʟʏ   ʟᴇғᴛ  !!."
+    text = f"sᴜᴄᴄᴇssғᴜʟʟʏ   ʟᴇғᴛ  !!."
     await message.reply_text(text)
     await app.leave_chat(chat_id=chat_id, delete=True)
 
