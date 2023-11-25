c1740d27683ad32ec09845af5ffdb9fcaa8b253f
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-25 15:31:08 +0530

    Update nightmode.py

--- DAXXMUSIC/plugins/misc/nightmode.py
+++ DAXXMUSIC/plugins/misc/nightmode.py
@@ -4,7 +4,7 @@ from DAXXMUSIC import app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram.types import ChatPermissions
-from DAXXMUSIC.utils.nightmodedb import nightdb,nightmode_on,nightmode_off,get_nightchats 
+from DAXXMUSIC.mongo.nightmodedb import nightdb,nightmode_on,nightmode_off,get_nightchats 
 
 
 
