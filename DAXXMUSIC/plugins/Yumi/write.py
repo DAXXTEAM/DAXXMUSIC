bf3b8f5c4b0586ef08439b6c4614f48c393fbf8d
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-26 08:52:18 +0530

    Update write.py

--- DAXXMUSIC/plugins/Yumi/write.py
+++ DAXXMUSIC/plugins/Yumi/write.py
@@ -15,7 +15,7 @@ async def handwrite(_, message: Message):
 
     caption = f"""
 sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
-✨ ᴡʀɪᴛᴛᴇɴ ʙʏ : [{BOT_NAME}](https://t.me/{BOT_USERNAME})
+✨ ᴡʀɪᴛᴛᴇɴ ʙʏ : [𝐘ᴜᴍɪᴋᴏᴏ](https://t.me/{BOT_USERNAME})
 🥀 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}
 """
     await m.delete()
