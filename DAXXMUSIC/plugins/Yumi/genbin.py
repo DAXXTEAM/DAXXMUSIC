c32e54a3bef07845e35b1a28a662e3fedca6df56
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-28 04:17:58 +0530

    Update genbin.py

--- DAXXMUSIC/plugins/Yumi/genbin.py
+++ DAXXMUSIC/plugins/Yumi/genbin.py
@@ -60,4 +60,4 @@ def generate(client, message):
     prefix = random.choice(VALID_PREFIXES)
     length = 6  # Standard credit card length
     card_number = generate_test_card_number(prefix, length)
-    message.reply_text(f"𝗬𝗢𝗨𝗥 𝗕𝗜𝗡 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗘𝗗  {card_number} ✅")
+    message.reply_text(f"𝗕𝗜𝗡 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗘𝗗\n" f"{card_number} ✅")
