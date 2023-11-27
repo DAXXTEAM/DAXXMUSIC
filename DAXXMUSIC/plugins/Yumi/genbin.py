9a7e551a67d86c7708a6092f049c379a86b67017
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-28 03:55:16 +0530

    Update genbin.py

--- DAXXMUSIC/plugins/Yumi/genbin.py
+++ DAXXMUSIC/plugins/Yumi/genbin.py
@@ -2,7 +2,12 @@ from pyrogram import Client, filters
 import random
 from DAXXMUSIC import app
 
-def luhn_check(card_number):
+# Constants
+VALID_PREFIXES = [4, 5, 6,3]  # VISA starts with 4, MasterCard with 5, Discover with 6
+
+
+
+def luhn_checksum(card_number):
     def digits_of(n):
         return [int(d) for d in str(n)]
     digits = digits_of(card_number)
@@ -10,18 +15,21 @@ def luhn_check(card_number):
     even_digits = digits[-2::-2]
     checksum = sum(odd_digits)
     for d in even_digits:
-        checksum += sum(digits_of(d*2))
-    return checksum % 10 == 0
+        checksum += sum(digits_of(d * 2))
+    return checksum % 10
 
-def generate_credit_card_number():
-    card_num = [random.randint(0, 9) for _ in range(6)] 
-    
-    for i in range(10): 
-        card_num[0] = i  
-        if luhn_check(int(''.join(map(str, card_num)))):
-            return '4' + ''.join(map(str, card_num)) 
-    return generate_credit_card_number() 
+def generate_test_card_number(prefix, length):
+    card_number = [random.randint(0, 9) for _ in range(length - len(str(prefix)) - 1)]
+    card_number.insert(0, str(prefix))
+    card_number = ''.join(map(str, card_number))
+    checksum = luhn_checksum(int(card_number) * 10)
+    return card_number + str((10 - checksum) % 10)
+
+
+
 @app.on_message(filters.command("genbin"))
-def send_credit_card_number(client, message):
-    card_number = generate_credit_card_number()
-    message.reply(f"Generated A Credit Card Number: {card_number}", quote=True)
+def generate(client, message):
+    prefix = random.choice(VALID_PREFIXES)
+    length = 6  # Standard credit card length
+    card_number = generate_test_card_number(prefix, length)
+    message.reply_text(f"Generated Test Card Number: {card_number}")
