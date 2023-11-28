1dbf44a3f6bf5f30fb5fe0070b3fdb1c7c58cb18
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-28 14:57:43 +0530

    Create password.py

---
+++ DAXXMUSIC/plugins/Yumi/password.py
@@ -0,0 +1,20 @@
+import random, os
+from pyrogram import Client, filters, enums 
+from DAXXMUSIC import app
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@app.on_message(filters.command(["genpassword", 'genpw']))
+async def password(bot, update):
+    message = await update.reply_text(text="Pʀᴏᴄᴇꜱꜱɪɴɢ..")
+    password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
+    if len(update.command) > 1:
+        qw = update.text.split(" ", 1)[1]
+    else:
+        ST = ["5", "7", "6", "9", "10", "12", "14", "8", "13"] 
+        qw = random.choice(ST)
+    limit = int(qw)
+    random_value = "".join(random.sample(password, limit))
+    txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url='https://t.me/YumikooBot?startgroup=true')]])
+    await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
