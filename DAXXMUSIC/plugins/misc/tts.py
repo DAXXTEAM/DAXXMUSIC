df7aa827572282f727e5b5f78cf25d38ff32ef1c
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-30 00:19:42 +0530

    Create tts.py

---
+++ DAXXMUSIC/plugins/misc/tts.py
@@ -0,0 +1,11 @@
+from pyrogram import Client, filters
+from gtts import gTTS
+from DAXXMUSIC import app
+
+
+@app.on_message(filters.command('tts'))
+def text_to_speech(client, message):
+    text = message.text.split(' ', 1)[1]
+    tts = gTTS(text=text, lang='en')
+    tts.save('speech.mp3')
+    client.send_audio(message.chat.id, 'speech.mp3')
