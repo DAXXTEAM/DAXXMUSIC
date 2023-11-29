939bf79d53e38bb988639d049c1d75fe61700bd0
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-30 00:27:51 +0530

    Update tts.py

--- DAXXMUSIC/plugins/misc/tts.py
+++ DAXXMUSIC/plugins/misc/tts.py
@@ -6,6 +6,6 @@ from DAXXMUSIC import app
 @app.on_message(filters.command('tts'))
 def text_to_speech(client, message):
     text = message.text.split(' ', 1)[1]
-    tts = gTTS(text=text, lang='en')
+    tts = gTTS(text=text, lang='hi')
     tts.save('speech.mp3')
     client.send_audio(message.chat.id, 'speech.mp3')
