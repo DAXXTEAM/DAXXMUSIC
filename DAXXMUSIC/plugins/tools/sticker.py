3f0ddac612ba184920f2afd91a11719cd7b6d627
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-30 16:46:10 +0530

    Update sticker.py

--- DAXXMUSIC/plugins/tools/sticker.py
+++ DAXXMUSIC/plugins/tools/sticker.py
@@ -43,27 +43,6 @@ async def upscale_image(client, message):
         print(f"**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ**: {e}")
         await message.reply_text("**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ**.")
 
-
-##########2222
-
-@app.on_message(filters.command("ups"))
-async def upscaleImages(_, message):
-    file = await getFile(message)
-    if file is None:
-        return await message.reply_text("Replay to an image?")
-    msg = await message.reply("Wait A Min.. Upscalling Your Image")
-    imageBytes = open(file,"rb").read()
-    os.remove(file)
-    upscaledImage = await UpscaleImages(imageBytes)
-    try:
-      await message.reply_document(open(upscaledImage,"rb"), caption=f"Upscaled By @{BOT_USERNAME}")
-      await msg.delete()
-      os.remove(upscaledImage)
-    except Exception as e:
-       await msg.edit(f"{e}")
-
-
-
 ######### sticker id
 
 @app.on_message(filters.command("packkang"))
