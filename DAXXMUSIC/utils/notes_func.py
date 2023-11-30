b28d80ef209b2ba2749d5f35e76d397761b37103
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-30 11:44:07 +0530

    Update notes_func.py

--- DAXXMUSIC/utils/notes_func.py
+++ DAXXMUSIC/utils/notes_func.py
@@ -6,6 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup
 from DAXXMUSIC import app
 from DAXXMUSIC.mongo.notesdb import GetNote
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from DAXXMUSIC.utils.msg_types import button_markdown_parser
 
 class NoteTypeMap(Enum):
     text = auto()
