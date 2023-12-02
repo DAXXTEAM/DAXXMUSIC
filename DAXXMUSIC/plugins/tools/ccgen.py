e4051d38de419f93fb2e6dfc25336ea55436060b
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2023-12-02 12:14:06 +0530

    Create ccgen.py

---
+++ DAXXMUSIC/plugins/tools/ccgen.py
@@ -0,0 +1,37 @@
+from ... import *
+from pyrogram import *
+from pyrogram.types import *
+
+
+@app.on_message(filters.command(["gen", "ccgen"], [".", "!", "/"]))
+async def gen_cc(client, message):
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "**Please Give Me a Bin To\nGenerate Cc ...**"
+        )
+    try:
+        await message.delete()
+    except:
+        pass
+    aux = await message.reply_text("**Generating ...**")
+    bin = message.text.split(None, 1)[1]
+    if len(bin) < 6:
+        return await aux.edit("**❌ Wrong Bin❗...**")
+    try:
+        resp = await api.ccgen(bin, 10)
+        cards = resp.liveCC
+        await aux.edit(f"""
+**💠 Some Live Generated CC:**
+
+`{cards[0]}`\n`{cards[1]}`\n`{cards[2]}`
+`{cards[3]}`\n`{cards[4]}`\n`{cards[5]}`
+`{cards[6]}`\n`{cards[7]}`\n`{cards[8]}`
+`{cards[9]}`\n
+
+**💳 Bin:** `{resp.results[0].bin}`
+**⏳ Time Took:** `{resp.took}`"""
+        )
+    except Exception as e:
+        return await aux.edit(f"**Error:** `{e}`")
+
+  
