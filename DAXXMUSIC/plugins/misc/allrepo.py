6fecb891bfc4805ea55d0d1ffc8fa5233be0232f
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-03 23:15:48 +0530

    Update allrepo.py

--- DAXXMUSIC/plugins/misc/allrepo.py
+++ DAXXMUSIC/plugins/misc/allrepo.py
@@ -17,12 +17,6 @@
 """
 
 
-
-from pyrogram import Client, filters
-import requests
-from DAXXMUSIC import app
-
-
 @app.on_message(filters.command("allrepo"))
 async def all_repo_command(client, message):
     try:
@@ -30,17 +24,17 @@ async def all_repo_command(client, message):
         if len(message.command) > 1:
             github_username = message.command[1]
 
-            # Fetch all repositories of the GitHub user
-            repo_links = get_all_repositories(github_username)
+            # Fetch information about all repositories of the GitHub user
+            repo_info = get_all_repository_info(github_username)
 
-            # Send the repository links as a reply
-            await message.reply_text(repo_links)
+            # Send the repository information as a reply
+            await message.reply_text(repo_info)
         else:
             await message.reply_text("Please enter a GitHub username after the /allrepo command.")
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
 
-def get_all_repositories(github_username):
+def get_all_repository_info(github_username):
     # Set up the GitHub API URL for user repositories
     github_api_url = f"https://api.github.com/users/{github_username}/repos"
 
@@ -49,6 +43,19 @@ def get_all_repositories(github_username):
     data = response.json()
 
     # Extract relevant information from the response
-    repo_links = "\n".join([f"{repo['full_name']}: {repo['html_url']}" for repo in data])
+    repo_info = "\n\n".join([
+        f"Repository: {repo['full_name']}\n"
+        f"Description: {repo['description']}\n"
+        f"Stars: {repo['stargazers_count']}\n"
+        f"Forks: {repo['forks_count']}\n"
+        f"URL: {repo['html_url']}"
+        for repo in data
+    ])
 
-    return repo_links
+    return repo_info
+
+
+
+
+
+########### COPY PASTE KRLO BUT CREDIT DEKE JANA BHOSDI WALO
