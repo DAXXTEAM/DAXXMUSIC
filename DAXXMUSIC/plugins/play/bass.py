f7142f34138a814a5c0b65da61e98adc0c2bc730
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-03 13:08:11 +0530

    Update bass.py

--- DAXXMUSIC/plugins/play/bass.py
+++ DAXXMUSIC/plugins/play/bass.py
@@ -36,7 +36,7 @@ def apply_bass_boost(audio_path):
     audio = AudioSegment.from_file(audio_path)
 
     # Apply bass boost effect (adjust the gain according to your preference)
-    boosted_audio = audio.low_pass_filter(100).high_pass_filter(30).apply_gain(10)
+    boosted_audio = audio.low_pass_filter(500).high_pass_filter(100).apply_gain(10)
 
     # Save the boosted audio as a temporary file
     boosted_audio_path = "yumiboost.mp3"
