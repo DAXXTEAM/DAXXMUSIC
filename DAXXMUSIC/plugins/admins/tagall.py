34fb8b6ea1f1b57c3266ee6c45d85086c24b924a
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-05 12:07:27 +0530

    Update tagall.py

--- DAXXMUSIC/plugins/admins/tagall.py
+++ DAXXMUSIC/plugins/admins/tagall.py
@@ -2,6 +2,7 @@ from DAXXMUSIC import app
 import asyncio
 import random
 from pyrogram import Client, filters
+from pyrogram.enums import ChatType, ChatMemberStatus
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import ChatPermissions
 
