278f75c594e7186f05340b2170f8b630e1a84237
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-09 02:38:52 +0530

    Create post.py

---
+++ DAXXMUSIC/plugins/sudo/post.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from DAXXMUSIC import app
+from config import OWNER_ID
+from pyrogram.types import Message
+
+
+@app.on_message(filters.command(["post"], prefixes=["/", "."]) & filters.user(OWNER_ID))
+async def copy_messages(_, message):
+
+    if message.reply_to_message:
+      
+        destination_group_id = -1001927107785 
+
+        
+        await message.reply_to_message.copy(destination_group_id)
+        await message.reply("𝗗𝗢𝗡𝗘✅")
