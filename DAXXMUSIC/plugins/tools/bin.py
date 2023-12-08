9fa67fc269dfe4ddee577878cca530d55077732c
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-08 21:56:35 +0530

    Update bin.py

--- DAXXMUSIC/plugins/tools/bin.py
+++ DAXXMUSIC/plugins/tools/bin.py
@@ -4,31 +4,30 @@ import requests
 from bs4 import BeautifulSoup as bs
 from DAXXMUSIC import app
 
-# Function to handle /da command
-@app.on_message(filters.command('bin'))
+
+@app.on_message(filters.command('ar'))
 def bin_check(client, message):
     try:
         # Extract BIN from the message
-        _BIN = message.text[len('/bin '):]
+        _BIN = message.text[len('/ar '):]
 
         # Make a request to Binchk API
         _res = requests.get(f'http://binchk-api.vercel.app/bin={_BIN}')
         res = _res.json()
 
         # Respond with the BIN information
-        message.reply_text(f'''
-𝗕𝗜𝗡: `{_BIN}`
-𝗕𝗿𝗮𝗻𝗱⇢ **{res["brand"]}**
-𝗧𝗬𝗣𝗘⇢ **{res["type"]}**
-𝗟𝗘𝗩𝗘𝗟⇢ **{res["level"]}**
-𝗕𝗔𝗡𝗞⇢ **{res["bank"]}**
-𝗣𝗛𝗢𝗡𝗘⇢ **{res["phone"]}**
-𝗙𝗟𝗔𝗚⇢ **{res["flag"]}**
-𝗖𝗨𝗥𝗥𝗘𝗡𝗖𝗬⇢ **{res["currency"]}**
-𝗖𝗢𝗨𝗡𝗧𝗬⇢ **{res["country"]}({res["code"]})**
-''')
+        message.reply_text(f"""
+BIN: {_BIN}
+Brand⇢ {res["brand"]}
+Type⇢ {res["type"]}
+Level⇢ {res["level"]}
+Bank⇢ {res["bank"]}
+Phone⇢ {res["phone"]}
+Flag⇢ {res["flag"]}
+Currency⇢ {res["currency"]}
+Country⇢ {res["country"]}({res["code"]})
+""")
     except Exception as e:
         # Handle exceptions, if any
-        print(f"""Please Give Me a Bin To
-Get Bin Details""")
-        message.reply_text("""🚫 Incorrect input. Please provide a 6-digit BIN number.""")
+        print(f"Error: {e}")
+        message.reply_text("An error occurred while processing your request.")
