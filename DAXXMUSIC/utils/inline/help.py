28fb5b9107f6e5696ae84d2ebff309219e647b28
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-11 01:02:15 +0530

    Update help.py

--- DAXXMUSIC/utils/inline/help.py
+++ DAXXMUSIC/utils/inline/help.py
@@ -143,7 +143,7 @@ def help_back_markup(_):
             [
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
-                    callback_data=f"settings_back_helper",
+                    callback_data=f"close",
                 ),
             ]
         ]
