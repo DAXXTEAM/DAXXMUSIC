41ca4eb41f0e7aa45a45f5db6f43a1282d39fdba
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-11 01:06:05 +0530

    Update help.py

--- DAXXMUSIC/utils/inline/help.py
+++ DAXXMUSIC/utils/inline/help.py
@@ -10,7 +10,7 @@ def help_pannel(_, START: Union[bool, int] = None):
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
+            callback_data=f"close",
         ),
     ]
     mark = second if START else first
@@ -143,7 +143,7 @@ def help_back_markup(_):
             [
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
-                    callback_data=f"close",
+                    callback_data=f"settings_back_helper",
                 ),
             ]
         ]
