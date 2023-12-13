0cb5ce3cff925bc967874942c2e03d4cd5f69408
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-13 16:11:24 +0530

    Update imposter.py

--- DAXXMUSIC/plugins/tools/imposter.py
+++ DAXXMUSIC/plugins/tools/imposter.py
@@ -86,7 +86,7 @@ async def chk_usr(_, message: Message):
             message.from_user.last_name,
         )
     if msg != "":
-        await message.reply_photo("https://telegra.ph/file/98c8d031b20663facc2d0.jpg", caption=msg)
+        await message.reply_photo("https://telegra.ph/file/6b0a0f76bf5660454ae89.jpg", caption=msg)
 
 
 @app.on_message(filters.group & filters.command("imposter") & ~filters.bot & ~filters.via_bot)
