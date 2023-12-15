a0ea13a4b946a9b0d68ea57753241d767bedaafe
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-15 23:58:38 +0530

    Update and rename randompic.py to pic.py

--- DAXXMUSIC/plugins/Yumi/pic.py
+++ DAXXMUSIC/plugins/Yumi/pic.py
@@ -24,6 +24,7 @@ SOFTWARE. **"""
 
 from pyrogram import Client, filters
 import requests
+from pyrogram.types import Message
 from io import BytesIO
 from DAXXMUSIC import app
 
@@ -46,3 +47,29 @@ def pic(client, message):
         message.reply_photo(random_pic)
     else:
         message.reply("Sorry, I couldn't get a random picture at the moment. 😔")
+
+
+
+#____________________________
+
+@app.on_message(filters.command("pic"))
+def pic_command(client, message: Message):
+    # Extract the name from the command
+    try:
+        name = message.command[1]
+    except IndexError:
+        client.send_message(message.chat.id, "Please provide a name after the /pic command.")
+        return
+
+    # Build the Unsplash URL with the provided name
+    unsplash_url = f"https://source.unsplash.com/500x500/?{name}"
+
+    # Send the image as a photo
+    try:
+        response = requests.get(unsplash_url)
+        if response.status_code == 200:
+            client.send_photo(message.chat.id, photo=unsplash_url, caption=f"Here's a picture related to {name}.")
+        else:
+            client.send_message(message.chat.id, "Failed to fetch image.")
+    except requests.RequestException as e:
+        client.send_message(message.chat.id, f"An error occurred: {str(e)}")        
