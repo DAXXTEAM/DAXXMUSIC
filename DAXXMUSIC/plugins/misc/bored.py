d565cd0008667f4af61a2a7c5b2b10e7710cf000
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-16 20:55:39 +0530

    Create bored.py

---
+++ DAXXMUSIC/plugins/misc/bored.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+import requests
+from DAXXMUSIC import app
+
+# URL for the Bored API
+bored_api_url = "https://apis.scrimba.com/bored/api/activity"
+
+
+# Function to handle /bored command
+@app.on_message(filters.command("bored", prefixes="/"))
+async def bored_command(client, message):
+    # Fetch a random activity from the Bored API
+    response = requests.get(bored_api_url)
+    if response.status_code == 200:
+        data = response.json()
+        activity = data.get("activity")
+        if activity:
+            # Send the activity to the user who triggered the command
+            await message.reply(f"𝗙𝗲𝗲𝗹𝗶𝗻𝗴 𝗯𝗼𝗿𝗲𝗱? 𝗛𝗼𝘄 𝗮𝗯𝗼𝘂𝘁:\n\n {activity}")
+        else:
+            await message.reply("Nᴏ ᴀᴄᴛɪᴠɪᴛʏ ғᴏᴜɴᴅ.")
+    else:
+        await message.reply("Fᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴀᴄᴛɪᴠɪᴛʏ.")
