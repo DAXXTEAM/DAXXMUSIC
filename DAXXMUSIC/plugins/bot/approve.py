c5e059e766f9f4b3b7c4b488a02d39d8a9d798e2
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-17 15:59:02 +0530

    Create approve.py

---
+++ DAXXMUSIC/plugins/bot/approve.py
@@ -0,0 +1,22 @@
+from DAXXMUSIC import app
+from os import environ
+from pyrogram import Client, filters
+from pyrogram.types import ChatJoinRequest
+
+
+# Extract environment variables or provide default values
+chat_id_env = environ.get("CHAT_ID")
+CHAT_ID = [int(app) for app in chat_id_env.split(",")] if chat_id_env else []
+
+TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
+APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
+
+# Define an event handler for chat join requests
+@app.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+async def autoapprove(client: app, message: ChatJoinRequest):
+    chat = message.chat  # Chat
+    user = message.from_user  # User
+    print(f"{user.first_name} Joined 🤝")  # Logs
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    if APPROVED == "on":
+        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
