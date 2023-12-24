12c1f485520c928d3459f2e138345f08d679d484
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-12-25 03:51:35 +0530

    Update invitelink.py

--- DAXXMUSIC/plugins/tools/invitelink.py
+++ DAXXMUSIC/plugins/tools/invitelink.py
@@ -1,26 +1,40 @@
 from DAXXMUSIC import app
 from pyrogram import Client, filters
 from pyrogram.errors import ChatAdminRequired, ChatNotModified, ChatIdInvalid, FloodWait, InviteHashExpired, UserNotParticipant
+from pyrogram.types import Message
 
-@app.on_message(filters.command("invitelink"))
-def get_invite_link(client, message):
-    if message.text:
+@app.on_message(filters.command("invitelink", prefixes="/"))
+async def link_command_handler(client: Client, message: Message):
+    if len(message.command) != 2:
+        await message.reply("Invalid usage. Correct format: /invitelink group_id")
+        return
+    group_id = message.command[1]
+
+    try:
+        chat = await client.get_chat(int(group_id))
         try:
-            if message.chat.type == 'private':
-                group_id = int(message.text.split(" ")[1])
-            else:
-                group_id = message.chat.id
-            
-            try:
-                invite_link = app.export_chat_invite_link(group_id)
-                message.reply_text(f"Here is the invite link for the group {group_id}: {invite_link}")
-            except (ChatAdminRequired, ChatNotModified, ChatIdInvalid, FloodWait, InviteHashExpired, UserNotParticipant) as e:
-                error_message = str(e)
-                if "not enough rights" in error_message.lower():
-                    message.reply_text("I don't have the necessary permissions to invite users to this group.")
-                elif "chat not found" in error_message.lower():
-                    message.reply_text("The specified group does not exist.")
-                else:
-                    message.reply_text(f"Error: {error_message}")
-        except (IndexError, ValueError):
-            message.reply_text("Invalid command. Please use /invitelink <group_id>")
+            invite_link = await client.export_chat_invite_link(chat.id)
+        except FloodWait as e:
+            await message.reply(f"FloodWait: {e.x} seconds. Retrying in {e.x} seconds.")
+            return
+        file_content = (
+            f"Group ID: {chat.id}\n"
+            f"Group Title: {chat.title}\n"
+            f"Members Count: {chat.members_count}\n"
+            f"Invite Link: {invite_link}"
+        )
+        file_name = f"group_info_{chat.id}.txt"
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write(file_content)
+        await client.send_document(
+            chat_id=message.chat.id,
+            document=file_name,
+            caption=f"Here is the information for the group {chat.title}"
+        )
+
+    except Exception as e:
+        await message.reply(f"Error: {str(e)}")
+
+    finally:
+        import os
+        os.remove(file_name)
