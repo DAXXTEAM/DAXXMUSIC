d1fcab310e923407a89c8e09426106259016982f
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-12-25 04:31:46 +0530

    Update invitelink.py

--- DAXXMUSIC/plugins/tools/invitelink.py
+++ DAXXMUSIC/plugins/tools/invitelink.py
@@ -1,40 +1,62 @@
 from DAXXMUSIC import app
 from pyrogram import Client, filters
+from pyrogram.errors import ChatIdInvalid
 from pyrogram.errors import ChatAdminRequired, ChatNotModified, ChatIdInvalid, FloodWait, InviteHashExpired, UserNotParticipant
-from pyrogram.types import Message
+import os
+import json
 
-@app.on_message(filters.command("invitelink", prefixes="/"))
+
+@app.on_message(filters.command("link", prefixes="/"))
 async def link_command_handler(client: Client, message: Message):
     if len(message.command) != 2:
-        await message.reply("Invalid usage. Correct format: /invitelink group_id")
+        await message.reply("Invalid usage. Correct format: /link group_id")
         return
+
     group_id = message.command[1]
+    file_name = f"group_info_{group_id}.txt"
 
     try:
         chat = await client.get_chat(int(group_id))
+
+        if chat is None:
+            await message.reply("Unable to get information for the specified group ID.")
+            return
+
         try:
             invite_link = await client.export_chat_invite_link(chat.id)
         except FloodWait as e:
             await message.reply(f"FloodWait: {e.x} seconds. Retrying in {e.x} seconds.")
             return
-        file_content = (
-            f"Group ID: {chat.id}\n"
-            f"Group Title: {chat.title}\n"
-            f"Members Count: {chat.members_count}\n"
-            f"Invite Link: {invite_link}"
-        )
-        file_name = f"group_info_{chat.id}.txt"
+
+        group_data = {
+            "id": chat.id,
+            "type": str(chat.type),
+            "title": chat.title,
+            "members_count": chat.members_count,
+            "description": chat.description,
+            "invite_link": invite_link,
+            "is_verified": chat.is_verified,
+            "is_restricted": chat.is_restricted,
+            "is_creator": chat.is_creator,
+            "is_scam": chat.is_scam,
+            "is_fake": chat.is_fake,
+            "dc_id": chat.dc_id,
+            "has_protected_content": chat.has_protected_content,
+        }
+
         with open(file_name, "w", encoding="utf-8") as file:
-            file.write(file_content)
+            for key, value in group_data.items():
+                file.write(f"{key}: {value}\n")
+
         await client.send_document(
             chat_id=message.chat.id,
             document=file_name,
-            caption=f"Here is the information for the group {chat.title}"
+            caption=f"Here is the information for the group \n{chat.title}"
         )
 
     except Exception as e:
         await message.reply(f"Error: {str(e)}")
 
     finally:
-        import os
-        os.remove(file_name)
+        if os.path.exists(file_name):
+            os.remove(file_name)
