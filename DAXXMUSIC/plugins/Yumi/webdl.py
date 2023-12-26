37c0deebc6f64ea139be2a121f7928497857dc8c
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-26 21:56:33 +0530

    ONLY FOR HTML FILE NOT A JS BACKWORDS

---
+++ DAXXMUSIC/plugins/Yumi/webdl.py
@@ -0,0 +1,49 @@
+import requests
+from pyrogram import Client, filters
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from DAXXMUSIC import app
+
+
+def download_website(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+
+    retries = Retry(total=5, backoff_factor=1, status_forcelist=[500, 502, 503, 504])
+    session = requests.Session()
+    session.mount('http://', HTTPAdapter(max_retries=retries))
+
+    try:
+        response = session.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return f"Failed to download source code. Status code: {response.status_code}"
+
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+
+
+# Handler for /webdl command to download website source code
+@app.on_message(filters.command("webdl"))
+def web_download(client, message):
+    # Check if the command has a URL attached
+    if len(message.command) == 1:
+        message.reply_text("Please enter a URL along with the /webdl command.")
+        return
+
+    # Get the URL after /webdl command
+    url = message.command[1]
+
+    source_code = download_website(url)
+    if source_code.startswith('An error occurred') or source_code.startswith('Failed to download'):
+        message.reply_text(source_code)
+    else:
+        # Save the source code to a file
+        with open('website.txt', 'w', encoding='utf-8') as file:
+            file.write(source_code)
+
+        # Reply with the file
+        message.reply_document(document='website.txt', caption=f"Source code of {url}")
