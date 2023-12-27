5743a08e5c0fb3ef1275ad3fbd7d96983af38e7d
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2023-12-27 17:24:41 +0530

    Update images.py

--- DAXXMUSIC/plugins/Yumi/images.py
+++ DAXXMUSIC/plugins/Yumi/images.py
@@ -19,7 +19,7 @@ async def pinterest(_, message):
      count = 0
 
      msg = await message.reply(f"sᴄʀᴀᴘɪɴɢ ɪᴍᴀɢᴇs ғʀᴏᴍ ᴘɪɴᴛᴇʀᴇᴛs...")
-     for url in images["images"][:6]:
+     for url in images["images"][:10]:
                   
           media_group.append(InputMediaPhoto(media=url))
           count += 1
