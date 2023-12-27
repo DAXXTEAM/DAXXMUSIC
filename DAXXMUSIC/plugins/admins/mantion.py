fabb49ad70e0e956e53b863efd01334652308cc9
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2023-12-27 17:36:32 +0530

    Update mantion.py

--- DAXXMUSIC/plugins/admins/mantion.py
+++ DAXXMUSIC/plugins/admins/mantion.py
@@ -6,7 +6,7 @@ from pyrogram import filters
 SPAM_CHATS = []
 
 
-@app.on_message(filters.command(["mantion", "all"]) | filters.command("@all", "") & filters.group)
+@app.on_message(filters.command(["mantion", "utag"]) | filters.command("@utag", "") & filters.group)
 async def tag_all_users(_,message): 
     replied = message.reply_to_message  
     if len(message.command) < 2 and not replied:
@@ -19,8 +19,8 @@ async def tag_all_users(_,message):
         async for m in app.get_chat_members(message.chat.id): 
             if message.chat.id not in SPAM_CHATS:
                 break       
-            usernum += 5
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usernum += 3
+            usertxt += f"\n⚘ [{m.user.first_name}](tg://user?id={m.user.id})\n"
             if usernum == 1:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(2)
@@ -40,8 +40,8 @@ async def tag_all_users(_,message):
             if message.chat.id not in SPAM_CHATS:
                 break 
             usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 5:
+            usertxt += f"\n⚘ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            if usernum == 3:
                 await app.send_message(message.chat.id,f'{text}\n{usertxt}')
                 await asyncio.sleep(2)
                 usernum = 0
@@ -51,7 +51,7 @@ async def tag_all_users(_,message):
         except Exception:
             pass        
            
-@app.on_message(filters.command("mantionoff") & ~filters.private)
+@app.on_message(filters.command("mantionoff", "utagstop") & ~filters.private)
 async def cancelcmd(_, message):
     chat_id = message.chat.id
     if chat_id in SPAM_CHATS:
@@ -64,3 +64,4 @@ async def cancelcmd(_, message):
     else :
         await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")  
         return       
+                
