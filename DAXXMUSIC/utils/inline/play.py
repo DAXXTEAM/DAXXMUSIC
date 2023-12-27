3e08c88821a544af7a8c3194b8a83150a95ff009
Anuj Kumar RoY <106427006+anujkumarroy@users.noreply.github.com>
2023-12-27 22:03:29 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "⚘—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—⚘————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——⚘———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———⚘——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————⚘—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————⚘————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————⚘———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————⚘——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————⚘—"
     else:
-        bar = "—————————◉"
+        bar = "—————————⚘"
     buttons = [
                 [
             InlineKeyboardButton(
@@ -68,14 +68,14 @@ def stream_markup_timer(_, chat_id, played, dur):
                  [
             InlineKeyboardButton(
 
-                text="ᴅᴇᴠ 💕",
+                text="ᴜᴘᴅᴀᴛᴇ",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="💌 ɢʀᴏᴜᴘ",
+                text="sᴜᴘᴘᴏʀᴛ",
 
                 url=f"{SUPPORT_CHAT}",
 
@@ -97,14 +97,14 @@ def stream_markup(_, chat_id):
                  [
             InlineKeyboardButton(
 
-                text="ᴅᴇᴠ 💕",
+                text="ᴜᴘᴅᴀᴛᴇ",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="💌 ɢʀᴏᴜᴘ",
+                text="sᴜᴘᴘᴏʀᴛ",
 
                 url=f"{SUPPORT_CHAT}",
 
