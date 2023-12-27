8cfc9cbfd3892577a6d6af3fc98fa1070c1ad1f1
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-12-27 18:52:45 +0530

    Update wishcute.py

--- DAXXMUSIC/plugins/Yumi/wishcute.py
+++ DAXXMUSIC/plugins/Yumi/wishcute.py
@@ -41,7 +41,7 @@ async def cute(_, message):
         user_id = message.reply_to_message.from_user.id
         user_name = message.reply_to_message.from_user.first_name
 
-    mention = f"{user_name} (tg://user?id={{str(user_id)}})"
+    mention = f"[{user_name}] (tg://user?id={{str(user_id)}})"
     mm = random.randint(1, 100)
     CUTE = f"🍑 {mention} {mm}% ᴄᴜᴛᴇ ʙᴀʙʏ🥀"
 
