41ce7f1e0f4de6700d9fcf4da6d8c21053904961
Anuj Kumar RoY <106427006+anujkumarroy@users.noreply.github.com>
2023-12-28 12:01:51 +0530

    Update ytsearch.py

--- DAXXMUSIC/plugins/bot/ytsearch.py
+++ DAXXMUSIC/plugins/bot/ytsearch.py
@@ -21,11 +21,11 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 5:
-            text += f"Judul: {results[i]['title']}\n"
-            text += f"Durasi: {results[i]['duration']}\n"
-            text += f"Views: {results[i]['views']}\n"
-            text += f"Channel: {results[i]['channel']}\n"
-            text += f"https://www.youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"๏ ᴊᴜᴅᴜʟ ➛ {results[i]['title']}\n"
+            text += f"๏ ᴅᴜʀᴀsɪ ➛ {results[i]['duration']}\n"
+            text += f"๏ ᴠɪᴇᴡs ➛ {results[i]['views']}\n"
+            text += f"๏ ᴄʜᴀɴɴᴇʟ ➛ {results[i]['channel']}\n"
+            text += f"๏ ➛ https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
