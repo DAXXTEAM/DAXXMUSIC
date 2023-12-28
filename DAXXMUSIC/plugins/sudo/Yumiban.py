49f737b557f7c4108b4a6fe3f267cbad9e79ca2a
Anik <135734362+anikalwayspro@users.noreply.github.com>
2023-12-28 16:56:10 +0530

    Update Yumiban.py

--- DAXXMUSIC/plugins/sudo/Yumiban.py
+++ DAXXMUSIC/plugins/sudo/Yumiban.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["umi","umiko"], prefixes=["y", "Y"]) & admin_filter)
+@app.on_message(filters.command(["izu","izuka"], prefixes=["s", "S"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
@@ -139,5 +139,4 @@ async def restriction_app(app :app, message):
                        )
                      )
                 await message.reply("demoted !")
-
 
