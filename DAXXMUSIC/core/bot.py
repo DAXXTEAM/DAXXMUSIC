26e0b7420b1abd17b9234385abe8665a8924b539
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2023-12-28 20:11:52 +0530

    Update bot.py

--- DAXXMUSIC/core/bot.py
+++ DAXXMUSIC/core/bot.py
@@ -28,7 +28,7 @@ class DAXX(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=f"<u><b>๏ {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ ➛</b><u>\n\n๏ ɪᴅ ➛ <code>{self.id}</code>\n๏ ɴᴀᴍᴇ ➛ {self.name}\n๏ ᴜsᴇʀɴᴀᴍᴇ ➛ @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
