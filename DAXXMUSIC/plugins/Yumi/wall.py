bd0bfb7fa0c223ed55afde4b47236026b9e6bced
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2023-12-30 16:08:02 +0530

    Create wall.py

---
+++ DAXXMUSIC/plugins/Yumi/wall.py
@@ -0,0 +1,51 @@
+from pyrogram import Client, filters
+import requests
+from pyrogram.types import Message
+from io import BytesIO
+from DAXXMUSIC import app
+
+# Fill these out with your credentials
+
+
+def get_random_picture():
+    response = requests.get('https://source.unsplash.com/random')
+    if response.status_code == 200:
+        return BytesIO(response.content)
+    else:
+        return None  # If something went wrong
+
+
+# Command handler to respond to /pic commands
+@app.on_message(filters.command("wall"))
+def pic(client, message):
+    random_pic = get_random_picture()
+    if random_pic:
+        message.reply_photo(random_pic)
+    else:
+        message.reply("Sorry, I couldn't get a random picture at the moment. 😔")
+
+
+
+#____________________________
+
+@app.on_message(filters.command("pic"))
+def pic_command(client, message: Message):
+    # Extract the name from the command
+    try:
+        name = message.command[1]
+    except IndexError:
+        client.send_message(message.chat.id, "Please provide a name after the /pic command.")
+        return
+
+    # Build the Unsplash URL with the provided name
+    unsplash_url = f"https://source.unsplash.com/500x500/?{name}"
+
+    # Send the image as a photo
+    try:
+        response = requests.get(unsplash_url)
+        if response.status_code == 200:
+            client.send_photo(message.chat.id, photo=unsplash_url, caption=f"Here's a picture related to {name}.")
+        else:
+            client.send_message(message.chat.id, "Failed to fetch image.")
+    except requests.RequestException as e:
+        client.send_message(message.chat.id, f"An error occurred: {str(e)}")        
