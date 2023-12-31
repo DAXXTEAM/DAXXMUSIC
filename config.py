00eaa9106aac8332e1eafb47d7478dc4e0703f1c
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2023-12-31 18:55:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/tinaarobot/NYKAA-MUSIC",
+    "https://github.com/tinaarobot/DAXXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
