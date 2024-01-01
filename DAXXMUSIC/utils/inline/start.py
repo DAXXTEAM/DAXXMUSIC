b85288ab63dc2cafb63a9ec8201ba7b2c7d168a8
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-01 20:18:46 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -29,6 +29,10 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
+            InlineKeyboardButton(text=_[sᴜᴘᴘᴏʀᴛ], url=f"https://t.me/the_friendz"),
+            InlineKeyboardButton(text=_[ʀᴇᴘᴏ], url=f"https://telegra.ph/file/e7196953649136d3f9b68.mp4"),
+        ],
+        [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
     ]
