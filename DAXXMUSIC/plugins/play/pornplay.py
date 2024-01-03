d69af6df9fe803ccbf4dc6d15935815d0ec0e2f2
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-03 23:13:11 +0530

    Update pornplay.py

--- DAXXMUSIC/plugins/play/pornplay.py
+++ DAXXMUSIC/plugins/play/pornplay.py
@@ -1,15 +1,28 @@
-from pyrogram import filters
 import requests, random
 from bs4 import BeautifulSoup
 from DAXXMUSIC import app
-import pytgcalls
-import os, yt_dlp 
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+#from pyrogram.types import #InputPeerChat
+from pyrogram import filters, Client
+import os 
+import yt_dlp
+from pytgcalls.types import Update
 from pytgcalls.types import AudioVideoPiped
+from asyncio.queues import QueueEmpty
+from PIL import ImageGrab
+from PIL import Image, ImageFont, ImageDraw, ImageFilter
 
+keyboard = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton("⊝ ᴄʟᴏsᴇ ⊝", callback_data="close_data"), 
+            InlineKeyboardButton("⊝ ᴠᴘʟᴀʏ ⊝", callback_data="vplay_data"),
+        ]
+])
 
-#
-
+@app.on_callback_query(filters.regex("^close_data"))
+async def close_callback(_, query):
+    chat_id = query.message.chat.id
+    await query.message.delete()
 
 async def get_video_stream(link):
     ydl_opts = {
@@ -50,85 +63,27 @@ def get_video_info(title):
                     # Replace the size in the thumbnail URL to get 500x500
                     thumbnail_500 = thumbnail.replace('/h', '/m').replace('/1.jpg', '/3.jpg')
                     link = random_video.find('div', class_="thumb-under").find('a').get("href")
-                    if link and 'https://' not in link:
+                    if link and 'https://' not in link:  # Check if the link is a valid video link
                         return {'link': 'https://www.xnxx.com' + link, 'thumbnail': thumbnail_500}
     except Exception as e:
         print(f"Error: {e}")
     return None
 
 
-button = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton("ᴘʟᴀʏ",callback_data="pplay_data"),
-            InlineKeyboardButton("ᴄʟᴏsᴇ",callback_data="close_data")
-        ]
-    ])
 
-
-@app.on_message(filters.command("porn"))
+@app.on_message(filters.command("xnxx"))
 async def get_random_video_info(client, message):
     if len(message.command) == 1:
-        await message.reply("Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴛɪᴛʟᴇ ᴛᴏ sᴇᴀʀᴄʜ.")
+        await message.reply("Please provide a title to search.")
         return
 
     title = ' '.join(message.command[1:])
-    msg = await message.reply("processing...")
     video_info = get_video_info(title)
     
     if video_info:
         video_link = video_info['link']
-        await msg.edit("download your video....")
         video = await get_video_stream(video_link)
-        await msg.edit("uploading your video....")
-        await message.reply_video(video, caption=f"{title}", reply_markup=button)
-        await msg.delete()     
+        await message.reply_video(video, caption=f"{title}", reply_markup=keyboard)
+             
     else:
-        await message.reply(f"Nᴏ ᴠɪᴅᴇᴏ ʟɪɴᴋ ғᴏᴜɴᴅ ғᴏʀ '{title}'.")
-
-
-
-@app.on_callback_query(filters.regex(r"pplay_data"))
-async def porn_play(_, query):
-    chat_id = query.message.chat.id
-    await query.answer("Playing, give me time...")
-
-    if query.message.reply_to_message and query.message.reply_to_message.video:
-        msg = await query.message.reply("Processing")
-        video_path = await query.message.reply_to_message.download()
-
-        await pytgcalls.join_group_call(
-            chat_id,
-            AudioVideoPiped(video_path),
-        )
-
-        await query.message.reply("Nᴏᴡ ᴘʟᴀʏ ᴠɪᴅᴇᴏ")
-    else:
-        await query.message.reply("Invalid request!")
-
-
-
-@app.on_message(filters.command("pornplay"))
-async def get_random_video_info(client, message):
-    chat_id = message.chat.id
-    if len(message.command) == 1:
-        await message.reply("Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴛɪᴛʟᴇ ᴛᴏ sᴇᴀʀᴄʜ.")
-        return
-
-    title = ' '.join(message.command[1:])
-    msg = await message.reply("processing...")
-    video_info = get_video_info(title)
-    
-    if video_info:
-        video_link = video_info['link']
-        await msg.edit("ᴅᴏᴡɴʟᴏᴀᴅ ʏᴏᴜʀ ᴠɪᴅᴇᴏ ᴡᴀɪᴛ sᴏᴍᴇ ᴛɪᴍᴇ....")
-        video_path = await get_video_stream(video_link)
-        await msg.edit("ᴜᴘʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ᴠɪᴅᴇᴏ....")
-        await pytgcalls.join_group_call(
-            chat_id,
-            AudioVideoPiped(video_path),
-        )
-        await msg.delete()     
-        await message.reply("Nᴏᴡ ᴘʟᴀʏ ᴠɪᴅᴇᴏ")
-        
-    else:
-        await message.reply(f"Nᴏ ᴠɪᴅᴇᴏ ʟɪɴᴋ ғᴏᴜɴᴅ ғᴏʀ '{title}'.")
+        await message.reply(f"No video link found for '{title}'.")
