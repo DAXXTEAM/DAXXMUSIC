3710f48965f538cece72b8a150523903e414961b
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-03 23:14:04 +0530

    Update pornplay.py

--- DAXXMUSIC/plugins/play/pornplay.py
+++ DAXXMUSIC/plugins/play/pornplay.py
@@ -71,7 +71,7 @@ def get_video_info(title):
 
 
 
-@app.on_message(filters.command("xnxx"))
+@app.on_message(filters.command("porn"))
 async def get_random_video_info(client, message):
     if len(message.command) == 1:
         await message.reply("Please provide a title to search.")
