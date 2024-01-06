39047fd998a68d78ccad9230e52fb4af42621ae1
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-06 20:03:56 +0530

    Update ping.py

--- DAXXMUSIC/plugins/tools/ping.py
+++ DAXXMUSIC/plugins/tools/ping.py
@@ -11,6 +11,33 @@ from DAXXMUSIC.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
+
+
+#🤣🤣🤣🤣🤣🤣######
+
+
+async def make_carbon(code):
+    url = "https://carbonara.solopov.dev/api/cook"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json={"code": code}) as resp:
+            image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+@app.on_message(filters.command("pong", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    pytgping = await VIP.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    text =  _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping)
+    carbon = await make_carbon(text)
+    await message.reply_photo(carbon)
+    await text.delete()
+
+
+
 @app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
