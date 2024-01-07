5069adb3cce7067c3ca05871ebdc0d294a2c273b
piyush4014 <140847217+piyush4014@users.noreply.github.com>
2024-01-07 13:31:25 +0530

    Update password.py

--- DAXXMUSIC/plugins/Yumi/password.py
+++ DAXXMUSIC/plugins/Yumi/password.py
@@ -16,5 +16,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url='https://t.me/YumikooBot?startgroup=true')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url='https://t.me/DAISY_12_Bot?startgroup=true')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
