5524dc09fe7300bd4bc3c49e2b63634af6cb1ad7
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-07 11:29:41 +0530

    Update song.py

--- DAXXMUSIC/plugins/tools/song.py
+++ DAXXMUSIC/plugins/tools/song.py
@@ -9,44 +9,59 @@ from pyrogram.types import Message
 from youtubesearchpython import VideosSearch
 
 
-@app.on_message(filters.command(["song"], ["/", "!", "."]))
-async def song(client: app, message: Message):
-    aux = await message.reply_text("**🔄 𝐏𝐫𝐨𝐜𝐞𝐬𝐬𝐢𝐧𝐠 ...**")
-    if len(message.command) < 2:
-        return await aux.edit(
-            "**🤖 𝐆𝐢𝐯𝐞 🙃 𝐌𝐮𝐬𝐢𝐜 💿 𝐍𝐚𝐦𝐞 😍\n💞 𝐓𝐨 🔊 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 🥀 𝐒𝐨𝐧𝐠❗**"
-        )
-    try:
-        song_name = message.text.split(None, 1)[1]
-        vid = VideosSearch(song_name, limit = 1)
-        song_title = vid.result()["result"][0]["title"]
-        song_link = vid.result()["result"][0]["link"]
-        ydl_opts = {
-            "format": "mp3/bestaudio/best",
-            "verbose": True,
-            "geo-bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [
-                {
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": "mp3"
-                }
-            ],
-            "outtmpl": f"downloads/{song_title}",
-        }
-        await aux.edit("**𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠 ...**")
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(song_link)
-        await aux.edit("**𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠 ...**")
-        await message.reply_audio(f"downloads/{song_title}.mp3")
-        try:
-            os.remove(f"downloads/{song_title}.mp3")
-        except:
-            pass
-        await aux.delete()
-    except Exception as e:
-        await aux.edit(f"**Error:** {e}")
+# ------------------------------------------------------------------------------- #
 
+@app.on_message(filters.command("song"))
+def download_song(_, message):
+    query = " ".join(message.command[1:])  
+    print(query)
+    m = message.reply("**🔄 sᴇᴀʀᴄʜɪɴɢ... **")
+    ydl_ops = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+
+    except Exception as e:
+        m.edit("**⚠️ ɴᴏ ʀᴇsᴜʟᴛs ᴡᴇʀᴇ ғᴏᴜɴᴅ. ᴍᴀᴋᴇ sᴜʀᴇ ʏᴏᴜ ᴛʏᴘᴇᴅ ᴛʜᴇ ᴄᴏʀʀᴇᴄᴛ sᴏɴɢ ɴᴀᴍᴇ**")
+        print(str(e))
+        return
+    m.edit("**📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...**")
+    try:
+        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(float(dur_arr[i])) * secmul
+            secmul *= 60
+        m.edit("**📤 ᴜᴘʟᴏᴀᴅɪɴɢ...**")
+
+        message.reply_audio(
+            audio_file,
+            thumb=thumb_name,
+            title=title,
+            caption=f"{title}\n**ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ {message.from_user.mention}**",
+            duration=dur
+        )
+        m.delete()
+    except Exception as e:
+        m.edit(" - An error !!")
+        print(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
+
+# ------------------------------------------------------------------------------- #
 
 ###### INSTAGRAM REELS DOWNLOAD
 
