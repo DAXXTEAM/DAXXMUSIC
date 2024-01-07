2a84ea40b73d0b9cf2626c8722b46ff8862fbfc4
piyush4014 <140847217+piyush4014@users.noreply.github.com>
2024-01-07 17:42:39 +0530

    Update post.py

--- DAXXMUSIC/plugins/sudo/post.py
+++ DAXXMUSIC/plugins/sudo/post.py
@@ -9,7 +9,7 @@ async def copy_messages(_, message):
 
     if message.reply_to_message:
       
-        destination_group_id = -1001927107785 
+        destination_group_id = -1002053754663 
 
         
         await message.reply_to_message.copy(destination_group_id)
