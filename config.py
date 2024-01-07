fd3db1852ee00322b56f7f49bcd222fe8c970209
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-07 21:31:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import re
+qimport re
 from os import getenv
 
 from dotenv import load_dotenv
@@ -19,7 +19,7 @@ BOT_USERNAME = getenv("BOT_USERNAME" , "YumikooBot")
 BOT_NAME = getenv("BOT_NAME" , "Yumikoo")
 #get Your Assistant User name
 ASSUSERNAME = getenv("ASSUSERNAME" , "MissYumikoo")
-EVALOP = list(map(int, getenv("EVALOP", "2105971379 5360305806 6691393517").split()))
+EVALOP = list(map(int, getenv("EVALOP", "2105971379 5360305806 6664582540").split()))
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
@@ -29,7 +29,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", -1001802990747))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6691393517))
+OWNER_ID = int(getenv("OWNER_ID", 6664582540))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
