a220e851367bad1a57e684f4e663668458c5ce8f
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-07 22:16:27 +0530

    Update Yumiban.py

--- DAXXMUSIC/plugins/sudo/Yumiban.py
+++ DAXXMUSIC/plugins/sudo/Yumiban.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["axi","axiko"], prefixes=["n", "N"]) & admin_filter)
+@app.on_message(filters.command(["exi","exiko"], prefixes=["n", "N"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
