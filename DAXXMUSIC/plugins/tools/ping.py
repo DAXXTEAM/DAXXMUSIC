2dbbfbd96de5c590ceb0c708b5cf9c26bd300951
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-07 07:13:40 +0530

    Update ping.py

--- DAXXMUSIC/plugins/tools/ping.py
+++ DAXXMUSIC/plugins/tools/ping.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from DAXXMUSIC import app
-from DAXXMUSIC.core.call import DAXX
+from DAXXMUSIC.core.call import VIP
 from DAXXMUSIC.utils import bot_sys_stats
 from DAXXMUSIC.utils.decorators.language import language
 from DAXXMUSIC.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 import aiohttp
+import asyncio
 from io import BytesIO
 from DAXXMUSIC import app
 from pyrogram import filters
@@ -25,11 +27,54 @@ async def make_carbon(code):
 @app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
+    PING_IMG_URL = "https://telegra.ph/file/7fdbc4402341fed20fd63.jpg"
+    captionss = "**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ.**"
+    response = await message.reply_photo(PING_IMG_URL, caption=(captionss))
+    await asyncio.sleep(1)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ...**")
+    await asyncio.sleep(1)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ.**")
+    await asyncio.sleep(1)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ..**")
+    await asyncio.sleep(1.5)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ...**")
+    await asyncio.sleep(2)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ....**")
+    await asyncio.sleep(2)
+    await response.edit_caption("**📡sʏsᴛᴇᴍ ᴅᴀᴛᴀ ᴀɴᴀʟʏsᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ !**")
+    await asyncio.sleep(1.5)
+    await response.edit_caption("**📩sᴇɴᴅɪɴɢ sʏsᴛᴇᴍ ᴀɴᴀʟʏsᴇᴅ ᴅᴀᴛᴀ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...**")
     start = datetime.now()
-    pytgping = await DAXX.ping()
+    pytgping = await VIP.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     text =  _["ping_2"].format(resp, app.name, UP, RAM, CPU, DISK, pytgping)
     carbon = await make_carbon(text)
-    await message.reply_photo(carbon)
-    await text.delete()
+    captions = "**ㅤㅤ🏓 ᴘɪɴɢ...ᴘᴏɴɢ...ᴘɪɴɢ✨\nㅤㅤ🎸 ᴅɪɴɢ...ᴅᴏɴɢ...ᴅɪɴɢ💞**"
+    await message.reply_photo((carbon), caption=captions,
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        
+        ],
+        [
+            InlineKeyboardButton(
+                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/HEROKUFREECC",
+            ),
+            InlineKeyboardButton(
+                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/ALLTYPECC",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="❅ ʜᴇʟᴘ ❅", callback_data="settings_back_helper"
+            )
+        ],
+    ]
+    ),
+        )
+    await response.delete()
