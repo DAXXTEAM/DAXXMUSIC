5263d9c7335e31d56ff6ad4619772c321fb4e57d
piyush4014 <140847217+piyush4014@users.noreply.github.com>
2024-01-10 11:25:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/piyush/DAXXMUSIC",
+    "https://github.com/piyush4014/DAXXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
