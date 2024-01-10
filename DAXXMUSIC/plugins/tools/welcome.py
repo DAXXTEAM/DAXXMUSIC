4790bb65f92a1c4d1e811f11bed919f410a560fc
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2024-01-11 00:52:43 +0530

    Update welcome.py

--- DAXXMUSIC/plugins/tools/welcome.py
+++ DAXXMUSIC/plugins/tools/welcome.py
@@ -137,7 +137,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 ๏ ɴᴀᴍᴇ ➛ {user.mention}
 ๏ ɪᴅ ➛ {user.id}
 ๏ ᴜsᴇʀɴᴀᴍᴇ ➛ @{user.username}
-๏ ᴍᴀᴅᴇ ʙʏ ➛ [ʀᴏʏ-ᴇᴅɪᴛx](https://t.me/roy_editx)**
+๏ ᴍᴀᴅᴇ ʙʏ ➛ [ᴍʀ-ᴇᴅɪᴛx](https://t.me/mr_editx)**
 """,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"↻ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ↻", url=f"https://t.me/Nykaaxbot?startgroup=true")]])
         )
