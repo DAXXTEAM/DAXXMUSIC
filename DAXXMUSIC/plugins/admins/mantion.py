b45ca2ddab7ae011e0db529c67d469fea4048723
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-10 15:39:17 +0530

    Update mantion.py

--- DAXXMUSIC/plugins/admins/mantion.py
+++ DAXXMUSIC/plugins/admins/mantion.py
@@ -51,7 +51,7 @@ async def tag_all_users(_,message):
         except Exception:
             pass        
            
-@app.on_message(filters.command("mantionoff") & ~filters.private)
+@app.on_message(filters.command("alloff") & ~filters.private)
 async def cancelcmd(_, message):
     chat_id = message.chat.id
     if chat_id in SPAM_CHATS:
