aa3348a1e4d9dfb7e193ac693f093db0bf45df09
piyush4014 <140847217+piyush4014@users.noreply.github.com>
2024-01-10 13:52:35 +0530

    Update unbanall.py

--- DAXXMUSIC/plugins/admins/unbanall.py
+++ DAXXMUSIC/plugins/admins/unbanall.py
@@ -4,7 +4,7 @@ from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from DAXXMUSIC.utils.daxx_ban import admin_filter
 
-BOT_ID = "6196153811"
+BOT_ID = "6343014741"
 
 @app.on_message(filters.command("unbanall") & admin_filter)
 async def unban_all(_, msg):
