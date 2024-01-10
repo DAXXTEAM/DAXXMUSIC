0f3a304d91f36cad34b546c817283618816b55c1
piyush4014 <140847217+piyush4014@users.noreply.github.com>
2024-01-10 14:16:17 +0530

    Update write.py

--- DAXXMUSIC/plugins/Yumi/write.py
+++ DAXXMUSIC/plugins/Yumi/write.py
@@ -15,7 +15,7 @@ async def handwrite(_, message: Message):
 
     caption = f"""
 sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
-✨ ᴡʀɪᴛᴛᴇɴ ʙʏ : [𝐘ᴜᴍɪᴋᴏᴏ](https://t.me/{BOT_USERNAME})
+✨ ᴡʀɪᴛᴛᴇɴ ʙʏ : [​🇩​​🇦​​🇮​​🇸​​🇾​](https://t.me/{BOT_USERNAME})
 🥀 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}
 """
     await m.delete()
