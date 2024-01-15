910e2c8359ae866959ff5339d957d379a5711b4d
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 01:45:13 +0530

    Update video.py

--- DAXXMUSIC/plugins/Yumi/video.py
+++ DAXXMUSIC/plugins/Yumi/video.py
@@ -108,4 +108,4 @@ async def ytmusic(client, message: Message):
 __mod_name__ = "Vɪᴅᴇᴏ"
 __help__ = """ 
 /video to download video song
-/vsong to download video song """
+/yt to download video song """
