29a51b49daea8498e618e22c87fca40b075db475
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 02:18:01 +0530

    Update day.py

--- DAXXMUSIC/plugins/Yumi/day.py
+++ DAXXMUSIC/plugins/Yumi/day.py
@@ -6,7 +6,7 @@ from DAXXMUSIC import app
 @app.on_message(filters.command("day"))
 def date_to_day_command(client: Client, message: Message):
     try:
-        # Extract the date from the command message
+        # Extract the date from the command message......
         command_parts = message.text.split(" ", 1)
         if len(command_parts) == 2:
             input_date = command_parts[1].strip()
