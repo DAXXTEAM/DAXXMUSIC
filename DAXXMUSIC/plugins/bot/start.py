b3f4ac7e676668c06e76be71537d174db20fd123
LoveFeelingsWill <147972786+LoveFeelingsWill@users.noreply.github.com>
2024-01-18 18:07:28 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -105,9 +105,9 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        served_chats = len(await get_served_chats())
-        served_users = len(await get_served_users())
-        UP, CPU, RAM, DISK = await bot_sys_stats()
+       # served_chats = len(await get_served_chats())
+      #  served_users = len(await get_served_users())
+     #  UP, CPU, RAM, DISK = await bot_sys_stats()
         await message.reply_photo(
             random.choice(YUMI_PICS),
             caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM,served_users,served_chats),
