c0edcf7e8268067776081dc177677a5d320a443d
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-23 12:55:16 +0530

    Update joinleft.py

--- DAXXMUSIC/plugins/tools/joinleft.py
+++ DAXXMUSIC/plugins/tools/joinleft.py
@@ -1,43 +1,3 @@
-"""
-from DAXXMUSIC import app
-
-from pyrogram import filters
-from pyrogram.errors import (
-    RPCError, ChatAdminRequired
-)
-from pyrogram.types import ChatMemberUpdated
-
-# -------------
-
-
-@app.on_chat_member_updated(filters.group, group=20)
-async def member_has_left(client: app, member: ChatMemberUpdated):
-
-    if (
-        not member.new_chat_member
-        and member.old_chat_member.status not in {
-            "banned", "left", "restricted"
-        }
-        and member.old_chat_member
-    ):
-        pass
-    else:
-        return
-    user = (
-        member.old_chat_member.user
-        if member.old_chat_member
-        else member.from_user
-    )
-    try:
-        return await client.send_message(
-            chat_id=member.chat.id,
-            text=f"**Goodbye {user.mention}**"
-        )
-    except RPCError as e:
-        print(e)
-        return
-        
-"""
 from DAXXMUSIC import app
 from pyrogram import filters
 from pyrogram.errors import RPCError, ChatAdminRequired
@@ -62,7 +22,7 @@ async def member_has_left(client: app, member: ChatMemberUpdated):
     
     try:
         caption = f"Sᴀᴅ Tᴏ Sᴇᴇ Yᴏᴜ Lᴇᴀᴠɪɴɢ {user.mention}\nTᴀᴋᴇ Cᴀʀᴇ!"
-        left_gif = "https://telegra.ph/file/d28047520fad932521368.mp4"
+        left_gif = "https://graph.org/file/db6dec1e9bc0b14e9e842.mp4"
         
         await client.send_animation(
             chat_id=member.chat.id,
