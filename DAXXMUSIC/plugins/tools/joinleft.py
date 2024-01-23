a3a69e0ad9836e9ee7a7cddb4dd40ace467a42c9
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-23 12:22:32 +0530

    Update joinleft.py

--- DAXXMUSIC/plugins/tools/joinleft.py
+++ DAXXMUSIC/plugins/tools/joinleft.py
@@ -1,3 +1,4 @@
+"""
 from DAXXMUSIC import app
 
 from pyrogram import filters
@@ -36,3 +37,41 @@ async def member_has_left(client: app, member: ChatMemberUpdated):
         print(e)
         return
         
+"""
+from DAXXMUSIC import app
+from pyrogram import filters
+from pyrogram.errors import RPCError, ChatAdminRequired
+from pyrogram.types import ChatMemberUpdated
+
+# -------------
+
+
+@app.on_chat_member_updated(filters.group, group=20)
+async def member_has_left(client: app, member: ChatMemberUpdated):
+    if (
+        not member.new_chat_member
+        and member.old_chat_member.status not in {"banned", "left", "restricted"}
+        and member.old_chat_member
+    ):
+        pass
+    else:
+        return
+
+    user = (
+        member.old_chat_member.user if member.old_chat_member else member.from_user
+    )
+    
+    try:
+        formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        caption = f"Sᴀᴅ Tᴏ Sᴇᴇ Yᴏᴜ Lᴇᴀᴠɪɴɢ {user.mention}\nTᴀᴋᴇ Cᴀʀᴇ! \nCurrent Date and Time: {formatted_datetime}"
+        
+        left_gif = "https://telegra.ph/file/d28047520fad932521368.mp4"
+        
+        await client.send_animation(
+            chat_id=member.chat.id,
+            animation=left_gif,
+            caption=caption
+        )
+    except RPCError as e:
+        print(e)
+        return
