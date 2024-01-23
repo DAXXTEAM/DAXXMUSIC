97b940d849b10c826cb5a9f89a3c25c800aa49ab
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-23 12:13:14 +0530

    Update joinleft.py

--- DAXXMUSIC/plugins/tools/joinleft.py
+++ DAXXMUSIC/plugins/tools/joinleft.py
@@ -6,40 +6,7 @@ from pyrogram.errors import (
 )
 from pyrogram.types import ChatMemberUpdated
 
-
-@app.on_chat_member_updated(filters.group, group=10)
-async def member_has_joined(client: app, member: ChatMemberUpdated):
-
-    if (
-        member.new_chat_member
-        and member.new_chat_member.status not in {
-            "banned", "left", "restricted"
-        }
-        and not member.old_chat_member
-    ):
-        pass
-    else:
-        return
-
-    user = (
-        member.new_chat_member.user
-        if member.new_chat_member
-        else member.from_user
-    )
-    try:
-        if user.is_app:
-            return
-    except ChatAdminRequired:
-        return
-    try:
-        await client.send_message(
-            chat_id=member.chat.id,
-            text=f"**Welcome {user.mention}**",
-        )
-    except RPCError as e:
-        print(e)
-        return
-
+# -------------
 
 
 @app.on_chat_member_updated(filters.group, group=20)
@@ -48,7 +15,7 @@ async def member_has_left(client: app, member: ChatMemberUpdated):
     if (
         not member.new_chat_member
         and member.old_chat_member.status not in {
-            "banned", "restricted"
+            "banned", "left", "restricted"
         }
         and member.old_chat_member
     ):
