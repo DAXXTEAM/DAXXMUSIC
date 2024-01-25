637e0b1a2d53da201beab9f07949da63e5519fb3
Mr Daxx <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-25 12:36:26 +0530

    Update Wel2.py

--- DAXXMUSIC/plugins/Yumi/Wel2.py
+++ DAXXMUSIC/plugins/Yumi/Wel2.py
@@ -9,8 +9,7 @@ import pickledb
 from telegram import *
 from telegram.ext import Updater, MessageHandler, CommandHandler
 from telegram.ext import *
-from telegram.ext.dispatcher import run_async
-
+from telegram.ext.dispatcher import *
 from config import BOT_USERNAME, BOT_TOKEN
 
 help_text = (
