5985a374780bfda4eb32ca232c521f421e6e471d
Mr Daxx <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-25 17:46:09 +0530

    Update thumbnails.py

--- DAXXMUSIC/utils/thumbnails.py
+++ DAXXMUSIC/utils/thumbnails.py
@@ -88,7 +88,7 @@ async def get_thumb(videoid):
         y2 = Ycenter + 250
         logo = youtube.crop((x1, y1, x2, y2))
         logo.thumbnail((520, 520), Image.ANTIALIAS)
-        logo = ImageOps.expand(logo, border=17, fill="pink")
+        logo = ImageOps.expand(logo, border=17, fill="Magenta")
         background.paste(logo, (50, 100))
         draw = ImageDraw.Draw(background)
         
