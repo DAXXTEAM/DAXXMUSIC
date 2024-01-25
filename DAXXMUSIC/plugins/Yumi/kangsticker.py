3d622745dc773584f24ca51ac5a40c40d351a337
Mr Daxx <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-25 23:18:40 +0530

    Update kangsticker.py

--- DAXXMUSIC/plugins/Yumi/kangsticker.py
+++ DAXXMUSIC/plugins/Yumi/kangsticker.py
@@ -17,13 +17,12 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from DAXXMUSIC import app
 from config import BOT_USERNAME
 from DAXXMUSIC.utils.errors import capture_err
-"""
+
 from DAXXMUSIC.utils.files import (
     get_document_from_file_id,
     resize_file_to_sticker_size,
     upload_document,
 )
-"""
 
 from DAXXMUSIC.utils.stickerset import (
     add_sticker_to_set,
