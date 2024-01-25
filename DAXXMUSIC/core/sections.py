72ea2f9be186329ebb66bd32b3f6800439a680a5
Mr Daxx <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-25 21:44:01 +0530

    Create sections.py

---
+++ DAXXMUSIC/core/sections.py
@@ -0,0 +1,29 @@
+n = "\n"
+w = " "
+
+bold = lambda x: f"**{x}:** "
+bold_ul = lambda x: f"**--{x}:**-- "
+
+mono = lambda x: f"`{x}`{n}"
+
+
+def section(
+    title: str,
+    body: dict,
+    indent: int = 2,
+    underline: bool = False,
+) -> str:
+    text = (bold_ul(title) + n) if underline else bold(title) + n
+
+    for key, value in body.items():
+        if value is not None:
+            text += (
+                indent * w
+                + bold(key)
+                + (
+                    (value[0] + n)
+                    if isinstance(value, list) and isinstance(value[0], str)
+                    else mono(value)
+                )
+            )
+    return text
