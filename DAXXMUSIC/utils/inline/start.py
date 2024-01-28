bfaf0e80fcf0e37d7aebf3f4320e0d131fead837
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2024-01-28 23:20:28 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_7"], url="https://te.legra.ph/file/24425a36dacd6fddb4a6d.mp4"),
+            InlineKeyboardButton(text=_["S_B_7"], url="https://graph.org/file/fd125a3550dc376f62797.mp4"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
