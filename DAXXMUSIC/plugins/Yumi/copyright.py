a59c0ca0b0cadd5e146cfb44e5875ca652c417e6
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-28 13:43:51 +0530

    Update copyright.py

--- DAXXMUSIC/plugins/Yumi/copyright.py
+++ DAXXMUSIC/plugins/Yumi/copyright.py
@@ -8,3 +8,17 @@ from pyrogram.types import Message
 async def delete_edited_messages(client, edited_message):
     await edited_message.delete()
 
+
+# -------------------------------
+
+async def delete_pdf_files(client, message):
+    if message.document and message.document.mime_type == "application/pdf":
+        warning_message = f"@{message.from_user.username} ᴍᴀᴀ ᴍᴀᴛ ᴄʜᴜᴅᴀ ᴘᴅғ ʙʜᴇᴊ ᴋᴇ,\n ʙʜᴏsᴀᴅɪᴋᴇ ᴄᴏᴘʏʀɪɢʜᴛ ʟᴀɢʏᴇɢᴀ \n\n ᴅᴇʟᴇᴛᴇ ᴋᴀʀ ᴅɪʏᴀ ᴍᴀᴅᴀʀᴄʜᴏᴅ.\n\n ᴀʙ @iam_daxx ʙʜᴀɪ ᴋᴇ ᴅᴍ ᴍᴇ ᴀᴘɴɪ ᴍᴜᴍᴍʏ ᴋᴏ ʙʜᴇᴊ ᴅᴇ 🍌🍌🍌."
+        await message.reply_text(warning_message)
+        await message.delete()
+    else:  
+        pass
+
+@app.on_message(filters.group & filters.document)
+async def message_handler(client, message):
+    await delete_pdf_files(client, message)
