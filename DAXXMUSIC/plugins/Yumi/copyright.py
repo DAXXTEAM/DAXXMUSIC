33f16f0552693ba80517f36b903bfcc8752f754b
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-28 13:20:59 +0530

    Update copyright.py

--- DAXXMUSIC/plugins/Yumi/copyright.py
+++ DAXXMUSIC/plugins/Yumi/copyright.py
@@ -8,3 +8,11 @@ from pyrogram.types import Message
 @app.on_edited_message(filters.group & ~filters.me)
 async def delete_edited_messages(client, edited_message):
     await edited_message.delete()
+
+
+@app.on_message(filters.group & filters.text & ~filters.me)
+async def delete_links_and_keywords(client, message):
+    keywords = ["NCERT", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt"]
+    
+    if any(keyword.lower() in message.text.lower() for keyword in keywords) or any(link in message.text.lower() for link in ["http", "https", "www."]):
+        await message.delete()
