cd73bd31fad1620a9ab473befaf0e94ad2fb7478
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-28 13:26:46 +0530

    Update copyright.py

--- DAXXMUSIC/plugins/Yumi/copyright.py
+++ DAXXMUSIC/plugins/Yumi/copyright.py
@@ -10,9 +10,17 @@ async def delete_edited_messages(client, edited_message):
     await edited_message.delete()
 
 
+
 @app.on_message(filters.group & filters.text & ~filters.me)
-async def delete_links_and_keywords(client, message):
-    keywords = ["NCERT", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt"]
-    
-    if any(keyword.lower() in message.text.lower() for keyword in keywords) or any(link in message.text.lower() for link in ["http", "https", "www."]):
-        await message.delete()
+async def delete_long_messages(client, message):
+    try:
+        if len(message.text.split()) >= 10:
+
+       
+            print(f"Deleted message from {message.from_user.username}: {message.text}")
+            
+            
+            await message.delete()
+    except Exception as e:
+        print(f"Error deleting message: {e}")
+        
