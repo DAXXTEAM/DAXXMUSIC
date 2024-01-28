bbda21e95eaf08f0c10840f8cc014b38056f0407
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-28 13:29:08 +0530

    Update copyright.py

--- DAXXMUSIC/plugins/Yumi/copyright.py
+++ DAXXMUSIC/plugins/Yumi/copyright.py
@@ -2,25 +2,9 @@ from pyrogram import Client, filters
 from DAXXMUSIC import app
 from pyrogram.types import Message
 
-
-
+# -----------------------------------------------------
 
 @app.on_edited_message(filters.group & ~filters.me)
 async def delete_edited_messages(client, edited_message):
     await edited_message.delete()
 
-
-
-@app.on_message(filters.group & filters.text & ~filters.me)
-async def delete_long_messages(client, message):
-    try:
-        if len(message.text.split()) >= 10:
-
-       
-            print(f"Deleted message from {message.from_user.username}: {message.text}")
-            
-            
-            await message.delete()
-    except Exception as e:
-        print(f"Error deleting message: {e}")
-        
