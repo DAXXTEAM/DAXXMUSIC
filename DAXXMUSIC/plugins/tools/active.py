790578f290a2ad72a3dcaa567e9b78b06dfc54ed
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-29 18:35:54 +0530

    Update active.py

--- DAXXMUSIC/plugins/tools/active.py
+++ DAXXMUSIC/plugins/tools/active.py
@@ -1,5 +1,5 @@
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from unidecode import unidecode
 
 from DAXXMUSIC import app
@@ -12,26 +12,41 @@ from DAXXMUSIC.utils.database import (
 )
 
 
+async def generate_join_link(chat_id: int):
+    invite_link = await app.export_chat_invite_link(chat_id)
+    return invite_link
+
+
+def ordinal(n):
+    suffix = ['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]
+    if 11 <= (n % 100) <= 13:
+        suffix = 'th'
+    return str(n) + suffix
+
+
 @app.on_message(filters.command(["activevc", "activevoice"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
     served_chats = await get_active_chats()
     text = ""
     j = 0
+    buttons = []
     for x in served_chats:
         try:
-            title = (await app.get_chat(x)).title
+            chat_info = await app.get_chat(x)
+            title = chat_info.title
+            invite_link = await generate_join_link(x)
         except:
             await remove_active_chat(x)
             continue
         try:
-            if (await app.get_chat(x)).username:
-                user = (await app.get_chat(x)).username
+            if chat_info.username:
+                user = chat_info.username
                 text += f"<b>{j + 1}.</b> <a href=https://t.me/{user}>{unidecode(title).upper()}</a> [<code>{x}</code>]\n"
             else:
-                text += (
-                    f"<b>{j + 1}.</b> {unidecode(title).upper()} [<code>{x}</code>]\n"
-                )
+                text += f"<b>{j + 1}.</b> {unidecode(title).upper()} [<code>{x}</code>]\n"
+            button_text = f"๏ ᴊᴏɪɴ {ordinal(j + 1)} ɢʀᴏᴜᴘ ๏"
+            buttons.append([InlineKeyboardButton(button_text, url=invite_link)])
             j += 1
         except:
             continue
@@ -40,6 +55,7 @@ async def activevc(_, message: Message):
     else:
         await mystic.edit_text(
             f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs :</b>\n\n{text}",
+            reply_markup=InlineKeyboardMarkup(buttons),
             disable_web_page_preview=True,
         )
 
@@ -50,20 +66,23 @@ async def activevi_(_, message: Message):
     served_chats = await get_active_video_chats()
     text = ""
     j = 0
+    buttons = []
     for x in served_chats:
         try:
-            title = (await app.get_chat(x)).title
+            chat_info = await app.get_chat(x)
+            title = chat_info.title
+            invite_link = await generate_join_link(x)
         except:
             await remove_active_video_chat(x)
             continue
         try:
-            if (await app.get_chat(x)).username:
-                user = (await app.get_chat(x)).username
+            if chat_info.username:
+                user = chat_info.username
                 text += f"<b>{j + 1}.</b> <a href=https://t.me/{user}>{unidecode(title).upper()}</a> [<code>{x}</code>]\n"
             else:
-                text += (
-                    f"<b>{j + 1}.</b> {unidecode(title).upper()} [<code>{x}</code>]\n"
-                )
+                text += f"<b>{j + 1}.</b> {unidecode(title).upper()} [<code>{x}</code>]\n"
+            button_text = f"๏ ᴊᴏɪɴ {ordinal(j + 1)} ɢʀᴏᴜᴘ ๏"
+            buttons.append([InlineKeyboardButton(button_text, url=invite_link)])
             j += 1
         except:
             continue
@@ -72,5 +91,6 @@ async def activevi_(_, message: Message):
     else:
         await mystic.edit_text(
             f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs :</b>\n\n{text}",
+            reply_markup=InlineKeyboardMarkup(buttons),
             disable_web_page_preview=True,
         )
