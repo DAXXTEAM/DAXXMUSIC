99080b6170ebcd4a46aee396fcdd68a2e75afaf0
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-29 08:30:21 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
+            InlineKeyboardButton(text="source", callback_data="gib_source"),
         ]
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
