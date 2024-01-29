42388a6d6c44390e3b5e1b4fdfbf886486ad939b
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-29 08:49:58 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -29,12 +29,12 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_10"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text="support chat",
-url=f"https://t.me/funny"),
+            InlineKeyboardButton(text="support chat", url=f"https://t.me/funny"),
             InlineKeyboardButton(text="source", callback_data="gib_source"),
-        ]
+        ],  # Added comma here
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
     ]
     return buttons
+
