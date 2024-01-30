b4a031162ec5e332a1c7562af8b585fa117734ec
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-30 10:08:11 +0530

    Update echo.py

--- DAXXMUSIC/plugins/tools/echo.py
+++ DAXXMUSIC/plugins/tools/echo.py
@@ -25,13 +25,9 @@ async def toggle_echo(client, message):
     else:
         await message.reply("Invalid command! Use /echo on or /echo off.")
 
-# Echo messages if echo is enabled for the chat
-@app.on_message(filters.group & ~filters.command("echo"))
-async def echo_message(client, message):
-    chat_id = message.chat.id
-    if chat_id in echo_status and echo_status[chat_id]:
-        await message.copy(chat_id)
 
 @app.on_message(filters.group & ~filters.bot)
 async def echo(client, message):
-    await message.reply(message.text)
+    chat_id = message.chat.id
+    if chat_id in echo_status and echo_status[chat_id]:
+    await message.reply(chat_id, message.text)
