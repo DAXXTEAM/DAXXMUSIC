e20ab199d5f9abe2bc8a81ef19b460bf69eaa695
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-30 09:41:29 +0530

    Update echo.py

--- DAXXMUSIC/plugins/tools/echo.py
+++ DAXXMUSIC/plugins/tools/echo.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
+from DAXXMUSIC.utils.daxx_ban import admin_filter
 from DAXXMUSIC import app
 
 echo_status = {}
 
 
-@app.on_message(filters.command("echo") & filters.group)
+@app.on_message(filters.command("echo") & filters.group & admin_filter)
 async def toggle_echo(client, message):
     chat_id = message.chat.id
     if chat_id not in echo_status:
