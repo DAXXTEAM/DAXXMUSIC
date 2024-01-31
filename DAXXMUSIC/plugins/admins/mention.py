4804e286936806d6b8cc2183eec07ce3f80a6c01
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-31 13:52:21 +0530

    Update and rename mantion.py to mention.py

--- DAXXMUSIC/plugins/admins/mention.py
+++ DAXXMUSIC/plugins/admins/mention.py
@@ -9,7 +9,7 @@ from DAXXMUSIC.utils.daxx_ban import admin_filter
 SPAM_CHATS = []
 
 
-@app.on_message(filters.command(["mantion", "all"]) & filters.group & admin_filter)
+@app.on_message(filters.command(["mention", "all"]) & filters.group & admin_filter)
 async def tag_all_users(_,message): 
     replied = message.reply_to_message  
     if len(message.command) < 2 and not replied:
