d4e5b53eb2b630ea50f8444f52582100733caa27
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-04 09:53:08 +0530

    Update nexio.py

--- DAXXMUSIC/plugins/sudo/nexio.py
+++ DAXXMUSIC/plugins/sudo/nexio.py
@@ -141,20 +141,20 @@ async def restriction_app(app :app, message):
                      )
                 await message.reply("demoted !")
 
-# :::;;;;;;;;;;;;;::::::::::::::::::::::
 
-for fullpromoted in data:
-            print(f"present {fullpromoted}")            
-            if fullpromoted in fullpromote:
-                await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
-                    can_change_info=True,
-                    can_invite_users=True,
-                    can_delete_messages=True,
-                    can_restrict_members=True,
-                    can_pin_messages=True,
-                    can_promote_members=True,
-                    can_manage_chat=True,
-                    can_manage_video_chats=True,
-                       )
-                     )
-                await message.reply("fullpromoted !")
+async def your_function():
+    for promoted in data:
+        print(f"present {fullpromoted}")            
+        if fullpromoted in fullpromote:
+            await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
+                can_change_info=True,
+                can_invite_users=True,
+                can_delete_messages=True,
+                can_restrict_members=True,
+                can_pin_messages=True,
+                can_promote_members=True,
+                can_manage_chat=True,
+                can_manage_video_chats=True,
+               )
+             )
+            await message.reply("fullpromoted !")
