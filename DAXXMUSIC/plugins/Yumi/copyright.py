58548840b75958866a1d041779f40a9f38ea5757
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-06 20:49:12 +0530

    Update copyright.py

--- DAXXMUSIC/plugins/Yumi/copyright.py
+++ DAXXMUSIC/plugins/Yumi/copyright.py
@@ -4,10 +4,15 @@ from pyrogram.types import Message
 
 # -----------------------------------------------------
 
+#@app.on_edited_message(filters.group & ~filters.me)
+#async def delete_edited_messages(client, edited_message):
+    #await edited_message.delete()
+
+
 @app.on_edited_message(filters.group & ~filters.me)
 async def delete_edited_messages(client, edited_message):
-    await edited_message.delete()
-
+    if edited_message.edit_date and not edited_message.text:
+        await edited_message.delete()
 
 # -------------------------------
 
