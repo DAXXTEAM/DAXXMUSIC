57e978092bf50480b4e47a267460ef8c0f355f0a
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-06 20:20:00 +0530

    Create joke.py

---
+++ DAXXMUSIC/plugins/tools/joke.py
@@ -0,0 +1,12 @@
+import requests
+from DAXXMUSIC import app
+
+
+JOKE_API_ENDPOINT = 'https://hindi-jokes-api.onrender.com/jokes?api_key=1a6d440e3f5971eecebceee818c2'
+
+@app.on_message(filters.command("hjoke"))
+async def joke(_, message):
+    response = requests.get(JOKE_API_ENDPOINT)
+    r = response.json()
+    joke_text = r['jokeContent']
+    await message.reply_text(joke_text)
