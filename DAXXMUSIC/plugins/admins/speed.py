daad3dc36bb3e2f35c7096b838e7b271e91baa5e
Mohitag403 <mohitag403@gmail.com>
2024-02-08 23:21:58 +0530

    Revert "Update speed.py"
    This reverts commit b2e4ab62c11d20dc2efeebcc0f6060f335d8cea2.

--- DAXXMUSIC/plugins/admins/speed.py
+++ DAXXMUSIC/plugins/admins/speed.py
@@ -22,16 +22,15 @@ checker = []
 async def playback(cli, message: Message, _, chat_id):
     playing = db.get(chat_id)
     if not playing:
-        return await app.send_message(message.chat.id, text=_["queue_2"])
+        return await message.reply_text(_["queue_2"])
     duration_seconds = int(playing[0]["seconds"])
     if duration_seconds == 0:
-        return await app.send_message(message.chat.id, text=_["admin_27"])
+        return await message.reply_text(_["admin_27"])
     file_path = playing[0]["file"]
     if "downloads" not in file_path:
-        return await app.send_message(message.chat.id, text=_["admin_27"])
+        return await message.reply_text(_["admin_27"])
     upl = speed_markup(_, chat_id)
-    return await app.send_message(
-        message.chat.id,
+    return await message.reply_text(
         text=_["admin_28"].format(app.mention),
         reply_markup=upl,
     )
@@ -39,50 +38,60 @@ async def playback(cli, message: Message, _, chat_id):
 
 @app.on_callback_query(filters.regex("SpeedUP") & ~BANNED_USERS)
 @languageCB
-async def del_back_playlist(client, callback_query, _):
-    callback_data = callback_query.data.strip()
+async def del_back_playlist(client, CallbackQuery, _):
+    callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     chat, speed = callback_request.split("|")
     chat_id = int(chat)
     if not await is_active_chat(chat_id):
-        return await callback_query.answer(_["general_5"], show_alert=True)
-    is_non_admin = await is_nonadmin_chat(callback_query.message.chat.id)
+        return await CallbackQuery.answer(_["general_5"], show_alert=True)
+    is_non_admin = await is_nonadmin_chat(CallbackQuery.message.chat.id)
     if not is_non_admin:
-        if callback_query.from_user.id not in SUDOERS:
-            admins = adminlist.get(callback_query.message.chat.id)
+        if CallbackQuery.from_user.id not in SUDOERS:
+            admins = adminlist.get(CallbackQuery.message.chat.id)
             if not admins:
-                return await callback_query.answer(_["admin_13"], show_alert=True)
+                return await CallbackQuery.answer(_["admin_13"], show_alert=True)
             else:
-                if callback_query.from_user.id not in admins:
-                    return await callback_query.answer(_["admin_14"], show_alert=True)
+                if CallbackQuery.from_user.id not in admins:
+                    return await CallbackQuery.answer(_["admin_14"], show_alert=True)
     playing = db.get(chat_id)
     if not playing:
-        return await callback_query.answer(_["queue_2"], show_alert=True)
+        return await CallbackQuery.answer(_["queue_2"], show_alert=True)
     duration_seconds = int(playing[0]["seconds"])
     if duration_seconds == 0:
-        return await callback_query.answer(_["admin_27"], show_alert=True)
+        return await CallbackQuery.answer(_["admin_27"], show_alert=True)
     file_path = playing[0]["file"]
     if "downloads" not in file_path:
-        return await callback_query.answer(_["admin_27"], show_alert=True)
-    checkspeed = playing[0].get("speed")
+        return await CallbackQuery.answer(_["admin_27"], show_alert=True)
+    checkspeed = (playing[0]).get("speed")
     if checkspeed:
         if str(checkspeed) == str(speed):
             if str(speed) == str("1.0"):
-                return await callback_query.answer(_["admin_29"], show_alert=True)
+                return await CallbackQuery.answer(
+                    _["admin_29"],
+                    show_alert=True,
+                )
     else:
         if str(speed) == str("1.0"):
-            return await callback_query.answer(_["admin_29"], show_alert=True)
+            return await CallbackQuery.answer(
+                _["admin_29"],
+                show_alert=True,
+            )
     if chat_id in checker:
-        return await callback_query.answer(_["admin_30"], show_alert=True)
+        return await CallbackQuery.answer(
+            _["admin_30"],
+            show_alert=True,
+        )
     else:
         checker.append(chat_id)
     try:
-        await callback_query.answer(_["admin_31"])
+        await CallbackQuery.answer(
+            _["admin_31"],
+        )
     except:
         pass
-    mystic = await app.send_message(
-        callback_query.message.chat.id,
-        text=_["admin_32"].format(callback_query.from_user.mention),
+    mystic = await CallbackQuery.edit_message_text(
+        text=_["admin_32"].format(CallbackQuery.from_user.mention),
     )
     try:
         await DAXX.speedup_stream(
@@ -94,15 +103,10 @@ async def del_back_playlist(client, callback_query, _):
     except:
         if chat_id in checker:
             checker.remove(chat_id)
-        return await app.send_message(
-            callback_query.message.chat.id,
-            text=_["admin_33"],
-            reply_markup=close_markup(_),
-        )
+        return await mystic.edit_text(_["admin_33"], reply_markup=close_markup(_))
     if chat_id in checker:
         checker.remove(chat_id)
-    await app.send_message(
-        callback_query.message.chat.id,
-        text=_["admin_34"].format(speed, callback_query.from_user.mention),
+    await mystic.edit_text(
+        text=_["admin_34"].format(speed, CallbackQuery.from_user.mention),
         reply_markup=close_markup(_),
-    )
+  )
