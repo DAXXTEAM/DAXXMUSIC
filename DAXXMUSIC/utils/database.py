8d7f0d145d0865c73bc2529f24ed5dcf9465c2e0
Mohitag403 <mohitag403@gmail.com>
2024-02-08 23:22:11 +0530

    Revert "Update database.py"
    This reverts commit 62fbf42cc2f45f2a310913907dbc897fcc75b26b.

--- DAXXMUSIC/utils/database.py
+++ DAXXMUSIC/utils/database.py
@@ -281,7 +281,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from DAXXMUSIC.core.userbot import assistants
+    from VIPMUSIC.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -294,7 +294,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from DAXXMUSIC.core.userbot import assistants
+    from VIPMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
