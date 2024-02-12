6585a1facd8f5c87ed332bb4227eec80626d5488
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-12 22:22:29 +0530

    Update chatgpt.py

--- DAXXMUSIC/plugins/tools/chatgpt.py
+++ DAXXMUSIC/plugins/tools/chatgpt.py
@@ -32,6 +32,29 @@ async def chat(app :app, message):
         await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ")        
 
 
+#.
+@app.on_message(filters.command(["aan" , ],  prefixes=["j","J"]))
+async def chat(app :app, message):
+    
+    try:
+        start_time = time.time()
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "**ʜᴇʟʟᴏ sɪʀ**\n**ᴇxᴀᴍᴘʟᴇ:-**`.ask How to set girlfriend ?`")
+        else:
+            a = message.text.split(' ', 1)[1]
+            MODEL = "gpt-3.5-turbo"
+            resp = openai.ChatCompletion.create(model=MODEL,messages=[{"role": "user", "content": a}],
+    temperature=0.2)
+            x=resp['choices'][0]["message"]["content"]
+            await message.reply_text(f"{x}")     
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ")        
+
+# 
+
+
 
 
 
