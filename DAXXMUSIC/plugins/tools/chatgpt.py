a4cb3d2e52930b1fd2188723c07e00d73c1787f9
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-12 22:43:24 +0530

    Update chatgpt.py

--- DAXXMUSIC/plugins/tools/chatgpt.py
+++ DAXXMUSIC/plugins/tools/chatgpt.py
@@ -4,12 +4,11 @@ from pyrogram import filters
 from DAXXMUSIC import app
 from pyrogram.enums import ChatAction, ParseMode
 from gtts import gTTS
+import requests, config
 
+openai.api_key = "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"#chatgpt
 
-
-openai.api_key = "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"
-
-
+api_key ="995e3267-53f1-496e-82e3-39754eab99dc" #deepai
 
 
 @app.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
@@ -32,7 +31,7 @@ async def chat(app :app, message):
         await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ")        
 
 
-#.
+
 @app.on_message(filters.command(["aan" , ],  prefixes=["j","J"]))
 async def chat(app :app, message):
     
@@ -52,12 +51,8 @@ async def chat(app :app, message):
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ")        
 
-# 
 
-
-
-
-
+# voice chatgpt ai
 
 @app.on_message(filters.command(["assis"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
 async def chat(app :app, message):
@@ -82,7 +77,62 @@ async def chat(app :app, message):
             
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ") 
+        
+        
 
 
+@app.on_message(filters.command(["deep" , ],  prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
+async def deepchat(app: app, message):
+    name = message.from_user.first_name
+    try:
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(f"Hello {name}\nPlease provide text after the /deep command.")
+        else:
+            a = message.text.split(' ', 1)[1]
 
-##### Bing
+            data = {
+                'text': a,  
+            }
+
+            headers = {
+                'api-key': api_key,
+            }
+
+            r = requests.post("https://api.deepai.org/api/text-generator", data=data, headers=headers)
+            response = r.json()
+            answer_text = response['output']
+            await message.reply_text(f"{answer_text}")
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ**: {e}")
+
+
+#####
+
+@app.on_message(filters.command(["aby" , ],  prefixes=["b","B"]))
+async def deepchat(app: app, message):
+    name = message.from_user.first_name
+    try:
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(f"Hello {name}\nPlease provide text after the /deep command.")
+        else:
+            a = message.text.split(' ', 1)[1]
+
+            data = {
+                'text': a,  
+            }
+
+            headers = {
+                'api-key': api_key,
+            }
+
+            r = requests.post("https://api.deepai.org/api/text-generator", data=data, headers=headers)
+            response = r.json()
+            answer_text = response['output']
+            await message.reply_text(f"{answer_text}")
+    except Exception as e:
+        await message.reply_text(f"ᴇʀʀᴏʀ: {e}")
+
+
+##       
