04d7500595ad53d38bfc55d8b7f1c3edc8d2e2be
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-13 02:08:06 +0530

    Update gayfun.py

--- DAXXMUSIC/plugins/misc/gayfun.py
+++ DAXXMUSIC/plugins/misc/gayfun.py
@@ -1,6 +1,10 @@
-from pyrogram import Client, filters
 from pyrogram.types import Message
 import random
+from pyrogram import Client, filters, idle
+import pyrogram, asyncio, random, time
+from pyrogram.errors import FloodWait
+from pyrogram.types import *
+import requests
 from DAXXMUSIC import app
 
 def calculate_gay_percentage():
@@ -27,3 +31,26 @@ def gay_calculator_command(client, message: Message):
 
     # Send the gay response as a message
     message.reply_text(f"Gay Percentage: {gay_percentage}%\n{gay_response}")
+
+
+
+
+@app.on_message(filters.command("logo"))
+async def logo(app, msg: Message):
+    if len(msg.command) == 1:
+       return await msg.reply_text("Usage:\n\n /logo daxx")
+    logo_name = msg.text.split(" ", 1)[1]
+    API = f"https://api.sdbots.tech/logohq?text={logo_name}"
+    req = requests.get(API).url
+    await msg.reply_photo(
+        photo=f"{req}")
+
+@app.on_message(filters.command("animelogo"))
+async def logo(app, msg: Message):
+    if len(msg.command) == 1:
+       return await msg.reply_text("Usage:\n\n /animelogo daxx")
+    logo_name = msg.text.split(" ", 1)[1]
+    API = f"https://api.sdbots.tech/anime-logo?name={logo_name}"
+    req = requests.get(API).url
+    await msg.reply_photo(
+        photo=f"{req}")
