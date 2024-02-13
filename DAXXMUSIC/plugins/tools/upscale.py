d2fa86bb6f3da473b38b1eda1f9ded8af31e1d91
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-14 00:24:33 +0530

    Create upscale.py

---
+++ DAXXMUSIC/plugins/tools/upscale.py
@@ -0,0 +1,44 @@
+import base64
+import httpx
+import os
+from pyrogram import filters
+from config import BOT_USERNAME
+from DAXXMUSIC import app
+from pyrogram import filters
+import pyrogram
+from uuid import uuid4
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+
+
+@app.on_message(filters.reply & filters.command("upscale"))
+async def upscale_image(app, message):
+    try:
+        if not message.reply_to_message or not message.reply_to_message.photo:
+            await message.reply_text("**ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ɪᴛ.**")
+            return
+
+        image = message.reply_to_message.photo.file_id
+        file_path = await app.download_media(image)
+
+        with open(file_path, "rb") as image_file:
+            f = image_file.read()
+
+        b = base64.b64encode(f).decode("utf-8")
+
+        async with httpx.AsyncClient() as http_client:
+            response = await http_client.post(
+                "https://api.qewertyy.me/upscale", data={"image_data": b}, timeout=None
+            )
+
+        with open("upscaled_image.png", "wb") as output_file:
+            output_file.write(response.content)
+
+        await client.send_document(
+            message.chat.id,
+            document="upscaled.png",
+            caption="**ʜᴇʀᴇ ɪs ᴛʜᴇ ᴜᴘsᴄᴀʟᴇᴅ ɪᴍᴀɢᴇ!**",
+        )
+
+    except Exception as e:
+        print(f"**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ**: {e}")
+        await message.reply_text("**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ**.")
