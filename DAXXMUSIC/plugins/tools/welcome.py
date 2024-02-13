a2e40e92ff1e0ddf4f150f727e075c112317529e
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2024-02-14 01:22:40 +0530

    Update welcome.py

--- DAXXMUSIC/plugins/tools/welcome.py
+++ DAXXMUSIC/plugins/tools/welcome.py
@@ -89,8 +89,8 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=110)
     welcome_font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=60)
-    draw.text((430, 470), f'ID: {id}', fill=(30, 30, 30), font=font)
-    pfp_position = (50, 90)
+    draw.text((430, 470), f'ID: {id}', fill=(10, 10, 10), font=font)
+    pfp_position = (49, 88)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
