44560955bd4e3a84ab48e4f5dc2f5fd944d7227e
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-13 10:56:48 +0530

    Update chatgpt.py

--- DAXXMUSIC/plugins/tools/chatgpt.py
+++ DAXXMUSIC/plugins/tools/chatgpt.py
@@ -8,7 +8,7 @@ import requests, config
 
 openai.api_key = "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"#chatgpt
 
-api_key ="995e3267-53f1-496e-82e3-39754eab99dc" #deepai
+api_key ="bcbddea0-f2bd-4b4e-af1d-225b30893fcd" #deepai
 
 
 @app.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
