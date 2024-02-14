603db9b9fc442de48b56f713943118ec3025f57d
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-14 23:48:47 +0530

    Update upscale.py

--- DAXXMUSIC/plugins/tools/upscale.py
+++ DAXXMUSIC/plugins/tools/upscale.py
@@ -1,6 +1,7 @@
 import base64
 import httpx
 import os
+import requests 
 from pyrogram import filters
 from config import BOT_USERNAME
 from DAXXMUSIC import app
@@ -42,3 +43,40 @@ async def upscale_image(app, message):
     except Exception as e:
         print(f"**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ**: {e}")
         await message.reply_text("**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ**.")
+
+
+# ------------
+
+
+waifu_api_url = 'https://api.waifu.im/search'
+
+# IAM_DAXX
+
+def get_waifu_data(tags):
+    params = {
+        'included_tags': tags,
+        'height': '>=2000'
+    }
+
+    response = requests.get(waifu_api_url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+@app.on_message(filters.command("waifu"))
+def waifu_command(client, message):
+    try:
+        tags = ['maid']  # You can customize the tags as needed
+        waifu_data = get_waifu_data(tags)
+
+        if waifu_data and 'images' in waifu_data:
+            first_image = waifu_data['images'][0]
+            image_url = first_image['url']
+            message.reply_photo(image_url)
+        else:
+            message.reply_text("No waifu found with the specified tags.")
+
+    except Exception as e:
+        message.reply_text(f"An error occurred: {str(e)}")
