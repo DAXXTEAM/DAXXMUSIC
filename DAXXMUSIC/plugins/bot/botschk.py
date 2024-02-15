813784c3c7f161f81758c48e78c386d7d1f748cd
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-15 14:53:46 +0530

    Update botschk.py

--- DAXXMUSIC/plugins/bot/botschk.py
+++ DAXXMUSIC/plugins/bot/botschk.py
@@ -1,4 +1,3 @@
-import asyncio
 import random
 from pyrogram import filters
 from DAXXMUSIC import app
@@ -10,24 +9,27 @@ from ...logging import LOGGER
 
 from DAXXMUSIC import app, userbot
 from DAXXMUSIC.core.userbot import *
+
+import asyncio
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from DAXXMUSIC import app
 from config import OWNER_ID
 
-BOT_LIST = ["IAM_DAXXBOT", "NexikoBot" , "GitWizardBot" , "stringseasonrobot" , "LivioXBot"]
-
-
+BOT_LIST = ["IAM_DAXXBOT", "NexikoBot", "GitWizardBot", "stringseasonrobot", "LivioXBot"]
 
 @app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
-async def bots_chk(app, message):
+async def bots_chk(_, message):
     msg = await message.reply_photo(photo="https://telegra.ph/file/4d303296e4fac9a40ea07.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
     response = "**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ**\n\n"
     for bot_username in BOT_LIST:
         try:
-            bot = await userbot.get_users(bot_username)
+            bot = await app.get_users(bot_username)
             bot_id = bot.id
             await asyncio.sleep(0.5)
-            bot_info = await userbot.send_message(bot_id, "/start")
+            bot_info = await app.send_message(bot_id, "/start")
             await asyncio.sleep(3)
-            async for bot_message in userbot.get_chat_history(bot_id, limit=1):
+            async for bot_message in app.get_chat_history(bot_id, limit=1):
                 if bot_message.from_user.id == bot_id:
                     response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
                 else:
@@ -36,3 +38,4 @@ async def bots_chk(app, message):
             response += f"╭⎋ {bot_username}\n╰⊚ **sᴛᴀᴛᴜs: ᴇʀʀᴏʀ ❌**\n"
     
     await msg.edit_text(response)
+                
