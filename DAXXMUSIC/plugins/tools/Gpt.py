67c37e4cb7b8d27ce20b9f999778c8de964ac8f7
Madara Shelby <140615675+MadaraShelby@users.noreply.github.com>
2024-02-21 15:15:07 +0530

    Add files via upload

---
+++ DAXXMUSIC/plugins/tools/Gpt.py
@@ -0,0 +1,40 @@
+import random
+import time
+import requests
+from DAXXMUSIC import app
+from config import BOT_USERNAME
+
+from pyrogram.enums import ChatAction, ParseMode
+from pyrogram import filters
+
+@app.on_message(filters.command(["chatgpt","ai","ask","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
+async def chat_gpt(bot, message):
+    try:
+        start_time = time.time()
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+
+        if len(message.command) < 2:
+            await message.reply_text(
+                "Example:\n\n/chatgpt Where is TajMahal?"
+            )
+        else:
+            a = message.text.split(' ', 1)[1]
+            response = requests.get(f'https://chatgpt.apinepdev.workers.dev/?question={a}')
+
+            try:
+                # Check if "results" key is present in the JSON response
+                if "answer" in response.json():
+                    x = response.json()["answer"]
+                    end_time = time.time()
+                    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
+                    await message.reply_text(
+                        f" {x}  ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @sinewyxbot",
+                        parse_mode=ParseMode.MARKDOWN
+                    )
+                else:
+                    await message.reply_text("No 'results' key found in the response.")
+            except KeyError:
+                # Handle any other KeyError that might occur
+                await message.reply_text("Error accessing the response.")
+    except Exception as e:
+        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
