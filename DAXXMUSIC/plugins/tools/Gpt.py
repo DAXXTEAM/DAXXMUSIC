7a12747cd9c1dfe02164085e35677c6364cf324c
Madara Shelby <140615675+MadaraShelby@users.noreply.github.com>
2024-02-21 15:16:55 +0530

    Update Gpt.py

--- DAXXMUSIC/plugins/tools/Gpt.py
+++ DAXXMUSIC/plugins/tools/Gpt.py
@@ -28,7 +28,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}  ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @sinewyxbot",
+                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @NexikoBot",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
