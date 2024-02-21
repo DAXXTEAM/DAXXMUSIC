4bfb7e7903d8fdd7808bb46892336e4c3f00c235
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-21 23:15:56 +0530

    Update Pwelcome.py

--- DAXXMUSIC/plugins/tools/Pwelcome.py
+++ DAXXMUSIC/plugins/tools/Pwelcome.py
@@ -75,7 +75,7 @@ async def greet_group(_, member: ChatMemberUpdated):
             user.photo.big_file_id, file_name=f"pp{user.id}.png"
         )
     except AttributeError:
-        pic = "DAXXMUSIC/assets/profilepic.jpg"
+        pic = "DAXXMUSIC/assets/upic.png"
     if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
         try:
             await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
