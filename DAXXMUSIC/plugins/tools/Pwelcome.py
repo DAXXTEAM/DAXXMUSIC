2843df8b259d7296d752889f1992857b74ff9c0f
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-21 23:19:12 +0530

    Update Pwelcome.py

--- DAXXMUSIC/plugins/tools/Pwelcome.py
+++ DAXXMUSIC/plugins/tools/Pwelcome.py
@@ -37,7 +37,7 @@ def circle(pfp, size=(450, 450)):
     return pfp
 
 def welcomepic(pic, user, chat, id, uname):
-    background = Image.open("DAXXMUSIC/assets/Welcome.png")
+    background = Image.open("DAXXMUSIC/assets/welcome.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize(
