e6de866c7be0bd373c1c4d9708000254c8afae96
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-22 20:53:37 +0530

    Create rankings.py

---
+++ DAXXMUSIC/plugins/bot/rankings.py
@@ -0,0 +1,108 @@
+from pyrogram import filters
+from pymongo import MongoClient
+from DAXXMUSIC import app
+from config import MONGO_DB_URI
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from typing import Union
+
+import random
+from pyrogram import Client
+import requests
+import os
+import time
+
+mongo_client = MongoClient(MONGO_DB_URI)
+db = mongo_client["natu_rankings"]
+collection = db["ranking"]
+
+user_data = {}
+
+today = {}
+
+MISHI = [
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg",
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+]
+
+# watcher
+@app.on_message(filters.group & filters.group, group=6)
+def today_watcher(_, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    if chat_id in today and user_id in today[chat_id]:
+        today[chat_id][user_id]["total_messages"] += 1
+    else:
+        if chat_id not in today:
+            today[chat_id] = {}
+        if user_id not in today[chat_id]:
+            today[chat_id][user_id] = {"total_messages": 1}
+        else:
+            today[chat_id][user_id]["total_messages"] = 1
+
+@app.on_message(filters.group & filters.group, group=11)
+def _watcher(_, message):
+    user_id = message.from_user.id    
+    user_data.setdefault(user_id, {}).setdefault("total_messages", 0)
+    user_data[user_id]["total_messages"] += 1    
+    collection.update_one({"_id": user_id}, {"$inc": {"total_messages": 1}}, upsert=True)
+
+# ------------------- ranks ------------------ #
+@app.on_message(filters.command("today"))
+async def today_(_, message):
+    chat_id = message.chat.id
+    if chat_id in today:
+        users_data = [(user_id, user_data["total_messages"]) for user_id, user_data in today[chat_id].items()]
+        sorted_users_data = sorted(users_data, key=lambda x: x[1], reverse=True)[:10]
+
+        if sorted_users_data:
+            response = "**✦ 📈 ᴛᴏᴅᴀʏ ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ**\n\n"
+            for idx, (user_id, total_messages) in enumerate(sorted_users_data, start=1):
+                try:
+                    user_name = (await app.get_users(user_id)).first_name
+                except:
+                    user_name = "Unknown"
+                user_info = f"**{idx}**.   {user_name} ➠ {total_messages}\n"
+                response += user_info
+            button = InlineKeyboardMarkup(
+                [[    
+                   InlineKeyboardButton("ᴏᴠᴇʀᴀʟʟ ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ", callback_data="overall"),
+                ]])
+            await message.reply_photo(random.choice(MISHI), caption=response, reply_markup=button)
+        else:
+            await message.reply_text("❅ ɴᴏ ᴅᴀᴛᴀ ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛᴏᴅᴀʏ.")
+    else:
+        await message.reply_text("❅ ɴᴏ ᴅᴀᴛᴀ ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛᴏᴅᴀʏ.")
+
+@app.on_message(filters.command("ranking"))
+async def ranking(_, message):
+    top_members = collection.find().sort("total_messages", -1).limit(10)
+
+    response = "**✦ 📈 ᴄᴜʀʀᴇɴᴛ ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ**\n\n"
+    for idx, member in enumerate(top_members, start=1):
+        user_id = member["_id"]
+        total_messages = member["total_messages"]
+        try:
+            user_name = (await app.get_users(user_id)).first_name
+        except:
+            user_name = "Unknown"
+
+        user_info = f"**{idx}**.   {user_name} ➠ {total_messages}\n"
+
+  
