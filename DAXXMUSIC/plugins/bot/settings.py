37669fca6191f5ec0b634e822b98c7bc6a15ddc7
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2024-02-29 00:55:04 +0530

    Update settings.py

--- DAXXMUSIC/plugins/bot/settings.py
+++ DAXXMUSIC/plugins/bot/settings.py
@@ -394,7 +394,7 @@ async def vote_change(client, CallbackQuery, _):
 @app.on_callback_query(filters.regex("gib_source"))
 async def gib_repo_callback(_, callback_query):
     await callback_query.edit_message_media(
-        media=InputMediaVideo("https://te.legra.ph/file/b5cde2e89ef3b5eac5f75.mp4", has_spoiler=True),
+        media=InputMediaVideo("https://telegra.ph/file/41ec4455034f1d8d634ff.mp4", has_spoiler=True),
         reply_markup=InlineKeyboardMarkup(
             [
                 [close_button]
