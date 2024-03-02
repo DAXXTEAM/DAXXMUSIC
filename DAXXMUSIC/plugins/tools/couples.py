a7edf4ec6c24644b1d673095b1c0ab9c4823650d
INDIAN CODER <98512400+Adityaranjan0918@users.noreply.github.com>
2024-03-02 20:23:17 +0530

    Update couples.py

--- DAXXMUSIC/plugins/tools/couples.py
+++ DAXXMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from DAXXMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="𓊈𒆜彡[𝐃𝚊𝚡𝚡 𝐂𝙲 𝐂𝙻𝚄𝙱 ]彡𒆜𓊉",
-            url=f"https://t.me/ALLTYPECC",
+            text="𝐈𝐍𝐃𝐈𝐀𝐍 𝐂𝐎𝐃𝐄𝐑𝐒",
+            url=f"https://t.me/the_ind_coders",
         ),
     ],
 ]
