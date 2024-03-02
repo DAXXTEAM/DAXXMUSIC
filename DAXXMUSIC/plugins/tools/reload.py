bd939c0f2bcfeafc532e9e750a95cac8feb8e168
INDIAN CODER <98512400+Adityaranjan0918@users.noreply.github.com>
2024-03-02 20:18:05 +0530

    Update reload.py

--- DAXXMUSIC/plugins/tools/reload.py
+++ DAXXMUSIC/plugins/tools/reload.py
@@ -118,7 +118,7 @@ async def help(client: Client, message: Message):
              [
                  [
                       InlineKeyboardButton(
-                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/iam_daxx")
+                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/itz_aditya_the_king")
                  ]
             ]
          ),
