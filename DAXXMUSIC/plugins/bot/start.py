3b0ca4928b447fb042b40b276cecbf42ea8c8438
INDIAN CODER <98512400+Adityaranjan0918@users.noreply.github.com>
2024-03-02 08:46:04 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -28,14 +28,8 @@ from strings import get_string
 
 #--------------------------
 
-NEXI_VID = [
-"https://telegra.ph/file/1a3c152717eb9d2e94dc2.mp4",
-"https://graph.org/file/ba7699c28dab379b518ca.mp4",
-"https://graph.org/file/83ebf52e8bbf138620de7.mp4",
-"https://graph.org/file/82fd67aa56eb1b299e08d.mp4",
-"https://graph.org/file/318eac81e3d4667edcb77.mp4",
-"https://graph.org/file/7c1aa59649fbf3ab422da.mp4",
-"https://graph.org/file/2a7f857f31b32766ac6fc.mp4",
+NEXI_IMG = [
+"https://telegra.ph/file/b74420175db7cff2e1d5f.jpg",
 
 ]
 
