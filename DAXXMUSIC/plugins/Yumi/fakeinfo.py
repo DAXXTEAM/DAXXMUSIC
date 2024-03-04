4ff461b7682ca35413190306efb8e2483cdd1619
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-03-04 09:55:56 +0530

    Update fakeinfo.py

--- DAXXMUSIC/plugins/Yumi/fakeinfo.py
+++ DAXXMUSIC/plugins/Yumi/fakeinfo.py
@@ -23,33 +23,45 @@ SOFTWARE.***
 """
 
 import requests
-from pyrogram import Client
-from pyrogram import filters
 from DAXXMUSIC import app
+from pyrogram import filters
 
 
-random_user_api_url = 'https://randomuser.me/api/'
+@app.on_message(filters.command("fake"))
+async def address(_, message):
+    query = message.text.split(maxsplit=1)[1].strip()
+    url = f"https://randomuser.me/api/?nat={query}"
+    response = requests.get(url)
+    data = response.json()
 
+    if "results" in data:
+        user_data = data["results"][0]
 
-@app.on_message(filters.command("fake", prefixes="/"))
-def generate_fake_user_by_country(client, message):
-    country_name = message.text.split("/fake ", maxsplit=1)[1]
-    
-    # Call the RandomUser API to get fake user information for the specified country
-    response = requests.get(f'{random_user_api_url}?nat={country_name}')
-    
-    if response.status_code == 200:
-        user_info = response.json()['results'][0]
-        # Extract user details
-        first_name = user_info['name']['first']
-        last_name = user_info['name']['last']
-        email = user_info['email']
-        country = user_info['location']['country']
-        state = user_info['location']['state']
-        city = user_info['location']['city']
-        street = user_info['location']['street']['name']
-        zip_code = user_info['location']['postcode']
-        # Reply with the generated fake user information for the specified country
-        message.reply_text(f"𝗡𝗔𝗠𝗘➪ {first_name} {last_name}\n\n𝗘𝗠𝗔𝗜𝗟➪ {email}\n\n𝗖𝗢𝗨𝗡𝗧𝗥𝗬➪ {country}\n\n𝗦𝗧𝗔𝗧𝗘➪ {state}\n\n𝗖𝗜𝗧𝗬: {city}\n\n𝗔𝗗𝗗𝗥𝗘𝗦𝗦:➪{street}\n\n𝗭𝗜𝗣 𝗖𝗢𝗗𝗘➪ {zip_code}")
+        
+        name = f"{user_data['name']['title']} {user_data['name']['first']} {user_data['name']['last']}"
+        address = f"{user_data['location']['street']['number']} {user_data['location']['street']['name']}" 
+        city = user_data['location']['city']
+        state = user_data['location']['state']
+        country = user_data['location']['country'] 
+        postal = user_data['location']['postcode']
+        email = user_data['email']
+        phone = user_data['phone']
+        picture_url = user_data['picture']['large']
+
+        
+        caption = f"""
+﹝⌬﹞**ɴᴀᴍᴇ** ⇢ {name}
+﹝⌬﹞**ᴀᴅᴅʀᴇss** ⇢ {address}
+﹝⌬﹞**ᴄᴏᴜɴᴛʀʏ** ⇢ {country}
+﹝⌬﹞**ᴄɪᴛʏ** ⇢ {city}
+﹝⌬﹞**sᴛᴀᴛᴇ** ⇢ {state}
+﹝⌬﹞**ᴘᴏsᴛᴀʟ** ⇢ {postal}
+﹝⌬﹞**ᴇᴍᴀɪʟ** ⇢ {email}
+﹝⌬﹞**ᴘʜᴏɴᴇ** ⇢ {phone}
+
+        """
+
+        
+        await message.reply_photo(photo=picture_url, caption=caption)
     else:
-        message.reply_text(f"Failed to generate fake user information for {country_name}.")
+        await message.reply_text("ᴏᴏᴘs ɴᴏᴛ ғᴏᴜɴᴅ ᴀɴʏ ᴀᴅᴅʀᴇss.")
