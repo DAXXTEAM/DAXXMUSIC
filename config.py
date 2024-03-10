7857d81cb2e469fc01d4050ddeb58315fd461164
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-03-10 21:23:12 +0530

    Update config.py
    .

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DAXXSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Yoursenpai002")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/rollahaibhaika")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
