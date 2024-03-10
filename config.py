9b69f2da4519e1213f3a6ed28cf26025b37c0838
insanekaneki7 <162685351+insanekaneki7@users.noreply.github.com>
2024-03-11 04:21:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DAXXSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/INSANE_NETWORK")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/insanesociety")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
