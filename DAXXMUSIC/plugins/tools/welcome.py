25e96e58e6b92c948db510553505625d81c13b8d
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2024-03-15 21:59:21 +0530

    Update welcome.py

--- DAXXMUSIC/plugins/tools/welcome.py
+++ DAXXMUSIC/plugins/tools/welcome.py
@@ -116,12 +116,12 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     #draw.text((630, 540), f'ID: {id}', fill=(255, 255, 255), font=font)
     #
  #   draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
-    draw.text((1630, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
+    draw.text((2999, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
 #    draw.text((630, 150), f"{chatname}", fill=(225, 225, 225), font=welcome_font)
   #  draw.text((630, 230), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
 
     #
-    pfp_position = (338, 321)
+    pfp_position = (332, 323)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
