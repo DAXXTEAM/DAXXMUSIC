b028b65ae6a1cd6dac42a0b77fc5ace39863d262
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-03-22 21:20:03 +0530

    Update yumidb.py

--- DAXXMUSIC/utils/yumidb.py
+++ DAXXMUSIC/utils/yumidb.py
@@ -12,7 +12,13 @@ from DAXXMUSIC.misc import SUDOERS
 
 COMMANDERS = [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
 
-async def user_has_permission(chat_title : str, chat_id: int, user_id: int, permission: str,bot=True) -> tuple[bool, str]:
+from typing import Tuple
+
+async def user_has_permission(chat_title: str, chat_id: int, user_id: int, permission: str, bot=True) -> Tuple[bool, str]:
+    
+
+
+#async def user_has_permission(chat_title : str, chat_id: int, user_id: int, permission: str,bot=True) -> tuple[bool, str]:
     try:
         if user_id in SUDORES:
             have_permission = True
