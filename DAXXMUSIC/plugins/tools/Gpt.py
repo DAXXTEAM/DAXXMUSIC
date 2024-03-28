b9981082c3ee46f9d460ed4501cddbc3078daf77
Code Bitey <136950154+codebitey@users.noreply.github.com>
2024-03-28 05:39:05 +0530

    Update Gpt.py

--- DAXXMUSIC/plugins/tools/Gpt.py
+++ DAXXMUSIC/plugins/tools/Gpt.py
@@ -28,7 +28,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @NexikoBot",
+                        f" {x}      ",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
