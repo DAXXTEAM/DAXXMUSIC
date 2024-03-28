ab494d11c7d7cd730c28ef4d6ca4702b95828798
Code Bitey <136950154+codebitey@users.noreply.github.com>
2024-03-28 05:41:34 +0530

    Update mustjoin.py

--- DAXXMUSIC/plugins/bot/mustjoin.py
+++ DAXXMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from DAXXMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "ALLTYPECC"
+MUST_JOIN = "NezukoUpdates"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
