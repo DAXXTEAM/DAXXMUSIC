c1bc4c91f6b25cfb074ba3b2f06c4e9b9681a70d
Code Bitey <136950154+codebitey@users.noreply.github.com>
2024-03-28 05:37:53 +0530

    Update couples.py

--- DAXXMUSIC/plugins/tools/couples.py
+++ DAXXMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from DAXXMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="𓊈𒆜彡[𝐃𝚊𝚡𝚡 𝐂𝙲 𝐂𝙻𝚄𝙱 ]彡𒆜𓊉",
-            url=f"https://t.me/ALLTYPECC",
+            text="𓊈𒆜彡[ 𝐍𝐞𝐳𝐮𝐤𝐨 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ]彡𒆜𓊉",
+            url=f"https://t.me/NezukuSupport",
         ),
     ],
 ]
