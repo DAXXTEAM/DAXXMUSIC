e3de3fd48acbc025151716d3b9889fe31788adf8
Certified Coders <rajnishmishraaa1@gmail.com>
2026-03-28 09:54:35 +0530

    Update mongo.py

--- DAXXMUSIC/core/mongo.py
+++ DAXXMUSIC/core/mongo.py
@@ -7,7 +7,7 @@ from ..logging import LOGGER
 LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
+    mongodb = _mongo_async_.Daxx
     LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
     LOGGER(__name__).error("Failed to connect to your Mongo Database.")
